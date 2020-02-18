bee33e153a18dbfa8886ef9099e637a386cd5f04
Christopher Faulet <cfaulet@haproxy.com>
2020-02-18 14:51:51 +0100

    BUG/MINOR: http-ana: Matching on monitor-uri should be case-sensitive
    The monitor-uri should be case-sensitive. In reality, the scheme and the host
    part are case-insensitives and only the path is case-sensive. But concretely,
    since the start, the matching on the monitor-uri is case-sensitive. And it is
    probably the expected behavior of almost all users.
    
    This patch must be backported as far as 1.9. For HAProxy 2.0 and 1.9, it must be
    applied on src/proto_htx.c.
    
    (cherry picked from commit d27689e952b09e1222c04b4cb238dbb5d797a5f2)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 55646bd98eb38c5e54b53536732b984166f3b77b)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 943ab4d62c48a8545b8b655c92d664c56ee6b19a)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/proto_htx.c
+++ src/proto_htx.c
@@ -337,7 +337,7 @@ int htx_wait_for_request(struct stream *s, struct channel *req, int an_bit)
 	 * the monitor-uri is defined by the frontend.
 	 */
 	if (unlikely((sess->fe->monitor_uri_len != 0) &&
-		     isteqi(htx_sl_req_uri(sl), ist2(sess->fe->monitor_uri, sess->fe->monitor_uri_len)))) {
+		     isteq(htx_sl_req_uri(sl), ist2(sess->fe->monitor_uri, sess->fe->monitor_uri_len)))) {
 		/*
 		 * We have found the monitor URI
 		 */
