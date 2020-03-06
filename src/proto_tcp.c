9fc7c8ab3d216465d64f7274dbd218b0fdb0736c
Christopher Faulet <cfaulet@haproxy.com>
2020-03-06 15:23:18 +0100

    BUG/MINOR: rules: Increment be_counters if backend is assigned for a silent-drop
    Backend counters must be incremented only if a backend was already assigned to
    the stream (when the stream exists). Otherwise, it means we are still on the
    frontend side.
    
    This patch may be backported as far as 1.6.
    
    (cherry picked from commit ddc005ae5783b6479292156b97db7b6fbdabf6a3)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 78b992f7bf1bf68dbd97782dcf4b89ea19203181)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit ef804e436faa99fbea27d18c55c2b7f6a80ac034)
    [wt: s/_HA_ATOMIC_ADD/HA_ATOMIC_ADD]
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit a84c11e7b9804d080e98b0f7dd2318a5c4dbebb6)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 0ea1cda295ded4c1da3e25d50ac1035683a6f607)
    [wt: place the "if" in front of the regular increment, no atomic add here]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/proto_tcp.c
+++ src/proto_tcp.c
@@ -1195,7 +1195,8 @@ static enum act_return tcp_exec_action_silent_drop(struct act_rule *rule, struct
 		channel_abort(&strm->res);
 		strm->req.analysers &= AN_REQ_FLT_END;
 		strm->res.analysers &= AN_RES_FLT_END;
-		strm->be->be_counters.denied_req++;
+		if (strm->flags & SF_BE_ASSIGNED)
+			strm->be->be_counters.denied_req++;
 		if (!(strm->flags & SF_ERR_MASK))
 			strm->flags |= SF_ERR_PRXCOND;
 		if (!(strm->flags & SF_FINST_MASK))
