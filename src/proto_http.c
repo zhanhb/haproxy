7259b91f01f5765225345876c71228fcd7add8d6
Willy Tarreau <w@1wt.eu>
2020-04-29 11:22:08 +0200

    BUG/MEDIUM: capture: capture-req/capture-res converters crash without a stream
    Since commit 5f940703b3 ("MINOR: log: Don't depends on a stream to process
    samples in log-format string") it has become quite obvious that a few sample
    fetch functions and converters were still heavily dependent on the presence
    of a stream without testing for it.
    
    The capture-req and capture-res converters were in this case and could
    crash the process if misused.
    
    This fix adds a check for the stream's existence, and should be backported
    to all stable versions up to 1.6.
    
    (cherry picked from commit 5575896ba1120f136e680fd669afb5b084faa530)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 909523621f634a8d8a1e70a3a5024238dc313b0d)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 422b3a16ec459b4d3c2d0194669871a1e9e59a31)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 60ca3c5e26f4c9bd034a5758ba98330c27e62ba7)
    [wt: applied to proto_http.c]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/proto_http.c
+++ src/proto_http.c
@@ -11794,7 +11794,7 @@ static int sample_conv_url_dec(const struct arg *args, struct sample *smp, void 
 
 static int smp_conv_req_capture(const struct arg *args, struct sample *smp, void *private)
 {
-	struct proxy *fe = strm_fe(smp->strm);
+	struct proxy *fe;
 	int idx, i;
 	struct cap_hdr *hdr;
 	int len;
@@ -11802,6 +11802,10 @@ static int smp_conv_req_capture(const struct arg *args, struct sample *smp, void
 	if (!args || args->type != ARGT_SINT)
 		return 0;
 
+	if (!smp->strm)
+		return 0;
+
+	fe = strm_fe(smp->strm);
 	idx = args->data.sint;
 
 	/* Check the availibity of the capture id. */
@@ -11835,7 +11839,7 @@ static int smp_conv_req_capture(const struct arg *args, struct sample *smp, void
 
 static int smp_conv_res_capture(const struct arg *args, struct sample *smp, void *private)
 {
-	struct proxy *fe = strm_fe(smp->strm);
+	struct proxy *fe;
 	int idx, i;
 	struct cap_hdr *hdr;
 	int len;
@@ -11843,6 +11847,10 @@ static int smp_conv_res_capture(const struct arg *args, struct sample *smp, void
 	if (!args || args->type != ARGT_SINT)
 		return 0;
 
+	if (!smp->strm)
+		return 0;
+
+	fe = strm_fe(smp->strm);
 	idx = args->data.sint;
 
 	/* Check the availibity of the capture id. */
