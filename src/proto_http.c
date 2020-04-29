27b2e31195acfd43a0149e02839a82c8257e1204
Willy Tarreau <w@1wt.eu>
2020-04-29 11:50:38 +0200

    BUG/MEDIUM: http: the "unique-id" sample fetch could crash without a steeam
    Since commit 5f940703b3 ("MINOR: log: Don't depends on a stream to process
    samples in log-format string") it has become quite obvious that a few sample
    fetch functions and converters were still heavily dependent on the presence
    of a stream without testing for it.
    
    The unique-id sample fetch function, if called without a stream, will result
    in a crash.
    
    This fix adds a check for the stream's existence, and should be backported
    to all stable versions up to 1.7.
    
    (cherry picked from commit a1062a4de8ba300979cc380b69b284f86dc13853)
    [wt: adjusted ctx: unique_id was a ptr not an ist in 2.1]
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit c000f0d1e1228dd64a00538a387e7de3290b0860)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit c1534624ac9b78d19400d637970bde755fb60f60)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 43302cbaf883fa5d85676838aa2a53131d9517ae)
    [wt: applied to proto_http.c]
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 6de109c37618d518ee0776ee9de5f38612cc82b7)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/proto_http.c
+++ src/proto_http.c
@@ -10462,6 +10462,9 @@ smp_fetch_uniqueid(const struct arg *args, struct sample *smp, const char *kw, v
 	if (LIST_ISEMPTY(&smp->sess->fe->format_unique_id))
 		return 0;
 
+	if (!smp->strm)
+		return 0;
+
 	if (!smp->strm->unique_id) {
 		if ((smp->strm->unique_id = pool_alloc2(pool2_uniqueid)) == NULL)
 			return 0;
