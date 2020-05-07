b967df17814759dd47b451053739e02ca606eaae
Tim Duesterhus <tim@bastelstu.be>
2020-05-07 19:21:31 +0200

    BUG/MINOR: cfgparse: Abort parsing the current line if an invalid \x sequence is encountered
    This fixes OSS Fuzz issue https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=21931.
    
    OSS Fuzz detected a hang on configuration parsing for a 200kB line with a large number of
    invalid escape sequences. Most likely due to the amounts of error output generated.
    
    This issue is very minor, because usually generated configurations are to be trusted.
    
    The bug exists since at the very least HAProxy 1.4. The patch may be backported if desired.
    
    (cherry picked from commit e6291956e7ad622dd2e4ac864caf05c93db417d3)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 6e9c16696edf6107e9cc377d539410f5de9198f9)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 9f244226fa143788b560c0c5fa5c0f6502ce4b86)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit af284435104fcf0202d846e7e7e407be853d979a)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/cfgparse.c
+++ src/cfgparse.c
@@ -7290,6 +7290,7 @@ next_line:
 					else {
 						ha_alert("parsing [%s:%d] : invalid or incomplete '\\x' sequence in '%s'.\n", file, linenum, args[0]);
 						err_code |= ERR_ALERT | ERR_FATAL;
+						goto next_line;
 					}
 				} else if (line[1] == '"') {
 					*line = '"';
