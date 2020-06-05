3def6371b1aafe4434497f2f3666095b35a56261
William Lallemand <wlallemand@haproxy.com>
2020-06-05 14:08:41 +0200

    BUG/MEDIUM: mworker: fix the reload with an -- option
    When HAProxy is started with a '--' option, all following parameters are
    considered configuration files. You can't add new options after a '--'.
    
    The current reload system of the master-worker adds extra options at the
    end of the arguments list. Which is a problem if HAProxy was started wih
    '--'.
    
    This patch fixes the issue by copying the new option at the beginning of
    the arguments list instead of the end.
    
    This patch must be backported as far as 1.8.
    
    (cherry picked from commit 0041741ef7253fcad2fc98f0b2a9968fb4af3574)
    [wla: context edit]
    Signed-off-by: William Lallemand <wlallemand@haproxy.org>
    (cherry picked from commit 29e710624583d71e7eeae63a6ea7734006a1ea57)
    Signed-off-by: William Lallemand <wlallemand@haproxy.org>
    (cherry picked from commit b9242bdd3d43f34033ac94333abe6ad567d5777a)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    [cfa: context edit]

--- src/haproxy.c
+++ src/haproxy.c
@@ -223,7 +223,7 @@ int shut_your_big_mouth_gcc_int = 0;
 
 int *children = NULL; /* store PIDs of children in master workers mode */
 
-static char **next_argv = NULL;
+static char **old_argv = NULL; /* previous argv but cleaned up */
 
 struct list proc_list = LIST_HEAD_INIT(proc_list);
 
@@ -724,6 +724,8 @@ static void get_cur_unixsocket()
  */
 void mworker_reload()
 {
+	char **next_argv = NULL;
+	int old_argc = 0; /* previous number of argument */
 	int next_argc = 0;
 	int j;
 	char *msg = NULL;
@@ -769,42 +771,45 @@ void mworker_reload()
 	}
 
 	/* compute length  */
-	while (next_argv[next_argc])
-		next_argc++;
+	while (old_argv[old_argc])
+		old_argc++;
 
 	/* 1 for haproxy -sf, 2 for -x /socket */
-	next_argv = realloc(next_argv, (next_argc + 1 + 2 + global.nbproc + nb_oldpids + 1) * sizeof(char *));
+	next_argv = calloc(old_argc + 1 + 2 +  global.nbproc + nb_oldpids + 1, sizeof(char *));
 	if (next_argv == NULL)
 		goto alloc_error;
 
+	/* copy the program name */
+	next_argv[next_argc++] = old_argv[0];
+
+	/* insert the new options just after argv[0] in case we have a -- */
 
 	/* add -sf <PID>*  to argv */
 	if (children || nb_oldpids > 0)
 		next_argv[next_argc++] = "-sf";
 	if (children) {
-		for (j = 0; j < global.nbproc; next_argc++,j++) {
-			next_argv[next_argc] = memprintf(&msg, "%d", children[j]);
-			if (next_argv[next_argc] == NULL)
+		for (j = 0; j < global.nbproc; j++) {
+			if ((next_argv[next_argc++] = memprintf(&msg, "%d", children[j])) == NULL)
 				goto alloc_error;
 			msg = NULL;
 		}
 	}
 	/* copy old process PIDs */
-	for (j = 0; j < nb_oldpids; next_argc++,j++) {
-		next_argv[next_argc] = memprintf(&msg, "%d", oldpids[j]);
-		if (next_argv[next_argc] == NULL)
+	for (j = 0; j < nb_oldpids; j++) {
+		if ((next_argv[next_argc++] = memprintf(&msg, "%d", oldpids[j])) == NULL)
 			goto alloc_error;
 		msg = NULL;
 	}
-	next_argv[next_argc] = NULL;
 
 	/* add the -x option with the stat socket */
 	if (cur_unixsocket) {
-
 		next_argv[next_argc++] = "-x";
 		next_argv[next_argc++] = (char *)cur_unixsocket;
-		next_argv[next_argc++] = NULL;
 	}
+
+	/* copy the previous options */
+	for (j = 1; j < old_argc; j++)
+		next_argv[next_argc++] = old_argv[j];
 
 	ha_warning("Reexecuting Master process\n");
 	signal(SIGPROF, SIG_IGN);
@@ -814,6 +819,8 @@ void mworker_reload()
 	return;
 
 alloc_error:
+	free(next_argv);
+	next_argv = NULL;
 	ha_warning("Failed to reexecute the master process [%d]: Cannot allocate memory\n", pid);
 	return;
 }
@@ -1523,8 +1530,8 @@ static void init(int argc, char **argv)
 	struct post_check_fct *pcf;
 
 	global.mode = MODE_STARTING;
-	next_argv = copy_argv(argc, argv);
-	if (!next_argv) {
+	old_argv = copy_argv(argc, argv);
+	if (!old_argv) {
 		ha_alert("failed to copy argv.\n");
 		exit(1);
 	}
