473272af5372b4eec546cf01fadbc7a660f82c0f
Willy Tarreau <w@1wt.eu>
2020-07-17 15:04:53 +0200

    BUILD: config: fix again bugs gcc warnings on calloc [try 2]
    Since commit ad37c7ab ("BUILD: config: address build warning on
    raspbian+rpi4") gcc 7.3.0 complains again on x86_64 (while 8.2.0
    does not) :
    
      src/cfgparse.c: In function 'check_config_validity':
      src/cfgparse.c:3593:26: warning: argument 1 range [18446744071562067968, 18446744073709551615] exceeds maximum object size 9223372036854775807 [-Walloc-size-larger-than=]
           newsrv->idle_conns = calloc(global.nbthread, sizeof(*newsrv->idle_conns));
                              ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    This thing is completely bogus (actually the RPi one was the most wrong).
    Let's try to shut them both by using an unsigned short for the cast which
    is expected to satisfy everyone. It's worth noting that the exact same call
    a few lines above and below do not trigger this stupid warning.
    
    This should be backported to 2.2 since the fix above was put there already.
    
    (cherry picked from commit 9b9c174e60ed8f518c13e99257ff2431aef3fa8a)
    [wt: this original patch from 2.4 only addressed idle_conns, but in
     2.2 we also have safe_conns and curr_idle_thr which cause the same
     warning that breaks the CI. Let's address them as well]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/cfgparse.c
+++ src/cfgparse.c
@@ -3579,7 +3579,7 @@ out_uri_auth_compat:
 				for (i = 0; i < global.nbthread; i++)
 					MT_LIST_INIT(&newsrv->idle_conns[i]);
 
-				newsrv->safe_conns = calloc(global.nbthread, sizeof(*newsrv->safe_conns));
+				newsrv->safe_conns = calloc((unsigned short)global.nbthread, sizeof(*newsrv->safe_conns));
 				if (!newsrv->safe_conns) {
 					ha_alert("parsing [%s:%d] : failed to allocate idle connections for server '%s'.\n",
 					    newsrv->conf.file, newsrv->conf.line, newsrv->id);
@@ -3590,7 +3590,7 @@ out_uri_auth_compat:
 				for (i = 0; i < global.nbthread; i++)
 					MT_LIST_INIT(&newsrv->safe_conns[i]);
 
-				newsrv->curr_idle_thr = calloc(global.nbthread, sizeof(*newsrv->curr_idle_thr));
+				newsrv->curr_idle_thr = calloc((unsigned short)global.nbthread, sizeof(*newsrv->curr_idle_thr));
 				if (!newsrv->curr_idle_thr)
 					goto err;
 				continue;
