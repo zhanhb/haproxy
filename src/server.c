d2098fcaa8d0f5b9910523947ec8fd4489b799fe
Amaury Denoyelle <adenoyelle@haproxy.com>
2020-11-03 18:34:13 +0100

    BUG/MINOR: server: fix down_time report for stats
    Adjust condition used to report down_time for statistics. There was a
    tiny probabilty to have a negative downtime if last_change was superior
    to now. If this is the case, return only down_time.
    
    This bug can backported up to 1.8.
    
    (cherry picked from commit e6ba7915ebdfb79d1d74eb402bbc2747c4a1c6dd)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 36f3e9bb61a06c1afce27474bbc357b52590ec96)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit dd491cd7393107b6f0300040c1bcca03b78c9d34)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 13ed5036df9fcfe8606e6d2eee56dba8269fc2d9)
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>

--- src/server.c
+++ src/server.c
@@ -59,7 +59,7 @@ static struct srv_kw_list srv_keywords = {
 
 int srv_downtime(const struct server *s)
 {
-	if ((s->cur_state != SRV_ST_STOPPED) && s->last_change < now.tv_sec)		// ignore negative time
+	if ((s->cur_state != SRV_ST_STOPPED) || s->last_change >= now.tv_sec)		// ignore negative time
 		return s->down_time;
 
 	return now.tv_sec - s->last_change + s->down_time;
