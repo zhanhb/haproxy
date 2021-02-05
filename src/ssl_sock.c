5c6176d283b2637bc9feb2a88c7a96b7c426998c
Willy Tarreau <w@1wt.eu>
2021-02-05 10:24:54 +0100

    BUILD: ssl: fix build breakage with last commit
    Bah I messed up the test of commit 5930fb23e ("BUG/MINOR: ssl: do not
    try to use early data if not configured") for 2.3, I ran regtest without
    rebuilding and naturally it wouldn't have built.
    
    Now fixed and tested again. If the commit above is backported further,
    this one will be needed as well.

--- src/ssl_sock.c
+++ src/ssl_sock.c
@@ -5061,7 +5061,7 @@ static int ssl_sock_init(struct connection *conn, void **xprt_ctx)
 		/* leave init state and start handshake */
 		conn->flags |= CO_FL_SSL_WAIT_HS | CO_FL_WAIT_L6_CONN;
 #ifdef SSL_READ_EARLY_DATA_SUCCESS
-		if (bc->ssl_conf.early_data)
+		if (__objt_listener(conn->target)->bind_conf->ssl_conf.early_data)
 			conn->flags |= CO_FL_EARLY_SSL_HS;
 #endif
 
