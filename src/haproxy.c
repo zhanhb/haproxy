019ec4f4a03ab84aab213eec7dd3746fd61d76ba
Christopher Faulet <cfaulet@haproxy.com>
2021-02-12 09:28:13 +0100

    CLEANUP: deinit: release global and per-proxy server-state variables on deinit
    The global server-state base directory and file name are now released on
    deinit, as well as per-proxy server-state file name.
    
    (cherry picked from commit f5ea269723a205c22d1ac9fd40b8d7fab5cb47ed)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 3dab6c0c9f345ee8dc72ee8f7072521210434cf4)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit ba56cfd29574d6015ce8eb9278a971758c8c1788)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 79c6a24b6752eef8dc26341ad9bbddde36877bb4)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 127bfb3717af6ac60d6cdf6a44f217e6bdc402d1)
    [ad: adjusted context]
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>

--- src/haproxy.c
+++ src/haproxy.c
@@ -2140,6 +2140,7 @@ void deinit(void)
 		free(p->cookie_domain);
 		free(p->cookie_attrs);
 		free(p->url_param_name);
+		free(p->server_state_file_name);
 		free(p->capture_name);
 		free(p->monitor_uri);
 		free(p->rdp_cookie_name);
@@ -2412,6 +2413,8 @@ void deinit(void)
 	free(global.desc);    global.desc = NULL;
 	free(oldpids);        oldpids = NULL;
 	task_free(global_listener_queue_task); global_listener_queue_task = NULL;
+	free(global.server_state_base); global.server_state_base = NULL;
+	free(global.server_state_file); global.server_state_file = NULL;
 
 	list_for_each_entry_safe(log, logb, &global.logsrvs, list) {
 			LIST_DEL(&log->list);
