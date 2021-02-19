6656972f9f9a186cba4fab95d5a1ad8d537d830e
Christopher Faulet <cfaulet@haproxy.com>
2021-02-19 16:47:11 +0100

    BUG/MINOR: server: Init params before parsing a new server-state line
    Same static arrays of parameters are used to parse all server-state
    lines. Thus it is important to reinit them to be sure to not get params from
    the previous line, eventually from the previous loaded file.
    
    This patch should be backported to all stable branches. However, in 2.0 and
    prior, the parsing of server-state lines are inlined in apply_server_state()
    function. Thus the patch will have to be adapted on these versions.
    
    (cherry picked from commit 06cd2569786d31ebb15832cb1053be9aaaadc3f7)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit d119e5671ace438224c51e6603d1e146981a224b)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 6018f67036b56c01c2a01f7054ac8128756373d5)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit a8170eb3fdea9b1d78b3e9ebd7690d45baf921e8)
    [cf: context adjustment]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/server.c
+++ src/server.c
@@ -3456,6 +3456,8 @@ void apply_server_state(void)
 			mybuf[mybuflen - 1] = '\0';
 
 			/* we're now ready to move the line into *srv_params[] */
+			memset(params, 0, SRV_STATE_FILE_MAX_FIELDS * sizeof(*params));
+			memset(srv_params, 0, SRV_STATE_FILE_MAX_FIELDS * sizeof(*srv_params));
 			params[0] = cur;
 			arg = 1;
 			srv_arg = 0;
