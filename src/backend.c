679e1b844275cfdad157a377a16f0476435e0af0
Amaury Denoyelle <adenoyelle@haproxy.com>
2021-06-17 15:14:49 +0200

    BUG/MINOR: backend: restore the SF_SRV_REUSED flag original purpose
    The SF_SRV_REUSED flag was set if a stream reused a backend connection.
    One of its purpose is to count the total reuse on the backend in
    opposition to newly instantiated connection.
    
    However, the flag was diverted from its original purpose since the
    following commit :
    
      e8f5f5d8b228d71333fb60229dc908505baf9222
      BUG/MEDIUM: servers: Only set SF_SRV_REUSED if the connection if fully ready.
    
    With this change, the flag is not set anymore if the mux is not ready
    when a connection is picked for reuse. This can happen for multiplexed
    connections which are inserted in the available list as soon as created
    in http-reuse always mode. The goal of this change is to not retry
    immediately this request in case on an error on the same server if the
    reused connection is not fully ready.
    
    This change is justified for the retry timeout handling but it breaks
    other places which still uses the flag for its original purpose. Mainly,
    in this case the wrong 'connect' backend counter is incremented instead
    of the 'reuse' one. The flag is also used in http_return_srv_error and
    may have an impact if a http server error is replied for this stream.
    
    To fix this problem, the original purpose of the flag is restored by
    setting it unconditionaly when a connection is reused. Additionally, a
    new flag SF_SRV_REUSED_ANTICIPATED is created. This flag is set when the
    connection is reused but the mux is not ready yet. For the timeout
    handling on error, the request is retried immediately only if the stream
    reused a connection without this newly anticipated flag.
    
    This must be backported up to 2.1.
    
    (cherry picked from commit 2b1d91758d74f53df3b9a1dbb1d8e627320b9532)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 05550a0113b1c19c207ccdd2237489d926cb1005)
    [wt: adjusted context; didn't backport the previously added BUG_ON()]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/backend.c
+++ src/backend.c
@@ -1492,11 +1492,9 @@ skip_reuse:
 			srv_conn->mux->reset(srv_conn);
 	}
 	else {
-		/* Only consider we're doing reuse if the connection was
-		 * ready.
-		 */
-		if (srv_conn->mux->ctl(srv_conn, MUX_STATUS, NULL) & MUX_STATUS_READY)
-			s->flags |= SF_SRV_REUSED;
+		s->flags |= SF_SRV_REUSED;
+		if (!(srv_conn->mux->ctl(srv_conn, MUX_STATUS, NULL) & MUX_STATUS_READY))
+			s->flags |= SF_SRV_REUSED_ANTICIPATED;
 	}
 
 	/* flag for logging source ip/port */
@@ -2177,6 +2175,8 @@ void back_handle_st_cer(struct stream *s)
 		 */
 
 		int delay = 1000;
+		const int reused = (s->flags & SF_SRV_REUSED) &&
+		                   !(s->flags & SF_SRV_REUSED_ANTICIPATED);
 
 		if (s->be->timeout.connect && s->be->timeout.connect < delay)
 			delay = s->be->timeout.connect;
@@ -2187,7 +2187,7 @@ void back_handle_st_cer(struct stream *s)
 		/* only wait when we're retrying on the same server */
 		if ((si->state == SI_ST_ASS ||
 		     (s->be->lbprm.algo & BE_LB_KIND) != BE_LB_KIND_RR ||
-		     (s->be->srv_act <= 1)) && !(s->flags & SF_SRV_REUSED)) {
+		     (s->be->srv_act <= 1)) && !reused) {
 			si->state = SI_ST_TAR;
 			si->exp = tick_add(now_ms, MS_TO_TICKS(delay));
 		}
