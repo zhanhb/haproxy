36a25d6ff04e4f82568cfa59287f3306747ed9e7
Tim Duesterhus <tim@bastelstu.be>
2021-08-28 23:57:01 +0200

    BUG/MINOR threads: Use get_(local|gm)time instead of (local|gm)time
    Using localtime / gmtime is not thread-safe, whereas the `get_*` wrappers are.
    
    Found using GitHub's CodeQL scan.
    
    The use in sample_conv_ltime() can be traced back to at least
    fac9ccfb705702f211f99e67d5f5d5129002086a (first appearing in 1.6-dev3), so all
    supported branches with thread support are affected.
    
    (cherry picked from commit 1f269c12dc31bb63db31559cb44c187ab91abb64)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 9da4b1fc83fe083c8f194c61402ba4ffeb5b330c)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 1ee21943ae572bcd9b54353ce2ceed5cb31e6332)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit a748b0a4bb63c5490581cc2dd9ae36915590f9c8)
    [wt: small ctx adjustment, no sub-second in 2.0]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/http_conv.c
+++ src/http_conv.c
@@ -43,7 +43,7 @@ static int sample_conv_http_date(const struct arg *args, struct sample *smp, voi
 	const char day[7][4] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
 	const char mon[12][4] = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 	struct buffer *temp;
-	struct tm *tm;
+	struct tm tm;
 	/* With high numbers, the date returned can be negative, the 55 bits mask prevent this. */
 	time_t curr_date = smp->data.u.sint & 0x007fffffffffffffLL;
 
@@ -51,16 +51,14 @@ static int sample_conv_http_date(const struct arg *args, struct sample *smp, voi
 	if (args && (args[0].type == ARGT_SINT))
 		curr_date += args[0].data.sint;
 
-	tm = gmtime(&curr_date);
-	if (!tm)
-		return 0;
+	get_gmtime(curr_date, &tm);
 
 	temp = get_trash_chunk();
 	temp->data = snprintf(temp->area, temp->size - temp->data,
 			      "%s, %02d %s %04d %02d:%02d:%02d GMT",
-			      day[tm->tm_wday], tm->tm_mday, mon[tm->tm_mon],
-			      1900+tm->tm_year,
-			      tm->tm_hour, tm->tm_min, tm->tm_sec);
+			      day[tm.tm_wday], tm.tm_mday, mon[tm.tm_mon],
+			      1900+tm.tm_year,
+			      tm.tm_hour, tm.tm_min, tm.tm_sec);
 
 	smp->data.u.str = *temp;
 	smp->data.type = SMP_T_STR;
