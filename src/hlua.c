42afc596c014312ec7354067cb38f8e8388d220c
Willy Tarreau <w@1wt.eu>
2021-08-30 09:35:18 +0200

    BUILD: ssl: fix two remaining occurrences of #if USE_OPENSSL
    One was in backend.c and the other one in hlua.c. No other candidate
    was found with "git grep '^#if\s*USE'". It's worth noting that 3
    other such tests exist for SSL_OP_NO_{SSLv3,TLSv1_1,TLSv1_2} but
    that these ones are properly set to 0 in openssl-compat.h when not
    defined.
    
    (cherry picked from commit b131049eb5ba5b0e7c02eea10ce9763da4428abe)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/hlua.c
+++ src/hlua.c
@@ -8593,7 +8593,7 @@ int hlua_post_init()
 	char *err = NULL;
 	struct hlua_function *fcn;
 
-#if USE_OPENSSL
+#if defined(USE_OPENSSL)
 	/* Initialize SSL server. */
 	if (socket_ssl->xprt->prepare_srv) {
 		int saved_used_backed = global.ssl_used_backend;
