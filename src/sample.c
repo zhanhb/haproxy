06f23c1fb97bfffe4ca7149d1bcab2845a6311f0
Willy Tarreau <w@1wt.eu>
2021-10-06 15:19:05 +0200

    CLEANUP: sample: rename sample_conv_var2smp() to *_sint
    This one only handles integers, contrary to its sibling with the suffix
    _str that only handles strings. Let's rename it and uninline it since it
    may well be used from outside.
    
    (cherry picked from commit d9be59952975e679f3305e32620ae3572a6a952c)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 9cfb71fb345c1c95377c0f2e72aff2a80aa10fc2)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 04a30d455d57e7e2dee4dd2efced078d1933f115)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit b2e6d4c8faa1be20aedcc5de4ca19e7746b02d6a)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/sample.c
+++ src/sample.c
@@ -2326,7 +2326,7 @@ static int check_operator(struct arg *args, struct sample_conv *conv,
  *
  * This function returns 0 if an error occurs, otherwise it returns 1.
  */
-static inline int sample_conv_var2smp(const struct arg *arg, struct sample *smp)
+int sample_conv_var2smp_sint(const struct arg *arg, struct sample *smp)
 {
 	switch (arg->type) {
 	case ARGT_SINT:
@@ -2363,7 +2363,7 @@ static int sample_conv_binary_and(const struct arg *arg_p, struct sample *smp, v
 	struct sample tmp;
 
 	smp_set_owner(&tmp, smp->px, smp->sess, smp->strm, smp->opt);
-	if (!sample_conv_var2smp(arg_p, &tmp))
+	if (!sample_conv_var2smp_sint(arg_p, &tmp))
 		return 0;
 	smp->data.u.sint &= tmp.data.u.sint;
 	return 1;
@@ -2377,7 +2377,7 @@ static int sample_conv_binary_or(const struct arg *arg_p, struct sample *smp, vo
 	struct sample tmp;
 
 	smp_set_owner(&tmp, smp->px, smp->sess, smp->strm, smp->opt);
-	if (!sample_conv_var2smp(arg_p, &tmp))
+	if (!sample_conv_var2smp_sint(arg_p, &tmp))
 		return 0;
 	smp->data.u.sint |= tmp.data.u.sint;
 	return 1;
@@ -2391,7 +2391,7 @@ static int sample_conv_binary_xor(const struct arg *arg_p, struct sample *smp, v
 	struct sample tmp;
 
 	smp_set_owner(&tmp, smp->px, smp->sess, smp->strm, smp->opt);
-	if (!sample_conv_var2smp(arg_p, &tmp))
+	if (!sample_conv_var2smp_sint(arg_p, &tmp))
 		return 0;
 	smp->data.u.sint ^= tmp.data.u.sint;
 	return 1;
@@ -2431,7 +2431,7 @@ static int sample_conv_arith_add(const struct arg *arg_p, struct sample *smp, vo
 	struct sample tmp;
 
 	smp_set_owner(&tmp, smp->px, smp->sess, smp->strm, smp->opt);
-	if (!sample_conv_var2smp(arg_p, &tmp))
+	if (!sample_conv_var2smp_sint(arg_p, &tmp))
 		return 0;
 	smp->data.u.sint = arith_add(smp->data.u.sint, tmp.data.u.sint);
 	return 1;
@@ -2446,7 +2446,7 @@ static int sample_conv_arith_sub(const struct arg *arg_p,
 	struct sample tmp;
 
 	smp_set_owner(&tmp, smp->px, smp->sess, smp->strm, smp->opt);
-	if (!sample_conv_var2smp(arg_p, &tmp))
+	if (!sample_conv_var2smp_sint(arg_p, &tmp))
 		return 0;
 
 	/* We cannot represent -LLONG_MIN because abs(LLONG_MIN) is greater
@@ -2479,7 +2479,7 @@ static int sample_conv_arith_mul(const struct arg *arg_p,
 	long long int c;
 
 	smp_set_owner(&tmp, smp->px, smp->sess, smp->strm, smp->opt);
-	if (!sample_conv_var2smp(arg_p, &tmp))
+	if (!sample_conv_var2smp_sint(arg_p, &tmp))
 		return 0;
 
 	/* prevent divide by 0 during the check */
@@ -2523,7 +2523,7 @@ static int sample_conv_arith_div(const struct arg *arg_p,
 	struct sample tmp;
 
 	smp_set_owner(&tmp, smp->px, smp->sess, smp->strm, smp->opt);
-	if (!sample_conv_var2smp(arg_p, &tmp))
+	if (!sample_conv_var2smp_sint(arg_p, &tmp))
 		return 0;
 
 	if (tmp.data.u.sint) {
@@ -2551,7 +2551,7 @@ static int sample_conv_arith_mod(const struct arg *arg_p,
 	struct sample tmp;
 
 	smp_set_owner(&tmp, smp->px, smp->sess, smp->strm, smp->opt);
-	if (!sample_conv_var2smp(arg_p, &tmp))
+	if (!sample_conv_var2smp_sint(arg_p, &tmp))
 		return 0;
 
 	if (tmp.data.u.sint) {
