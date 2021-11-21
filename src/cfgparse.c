zhanhb/haproxy

commit 8ab2a364a8c8adf0965e74a41a2ff3cebd43e7a9

MINOR: action: Use a generic function to check validity of an action rule list
The check_action_rules() function is now used to check the validity of an
action rule list. It is used from check_config_validity() function to check
L5/6/7 rulesets.

(cherry picked from commit 42c6cf950111736327863de5e82036a1d51deb04)
[cf: This patch is in fact a fix because the "tcp-resonse content" ruleset
     was not fully configured. It was ignored during Post-parsing
     stage. This patch should fix a bug reported in #1263 by @ HiggTh. It
     must be backported in all stable versions.]
Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
(cherry picked from commit dfc913347413f698901d7bd8d56b92c4ac9dd0a7)
Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
(cherry picked from commit 8015a6ef3c6dad39d26e47a92ea84fd93705159e)
Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

src/cfgparse.c
@@ -2823,65 +2823,13 @@ int check_config_validity()
 			}
 		}
 
-		/* check validity for 'tcp-request' layer 4 rules */
-		list_for_each_entry(arule, &curproxy->tcp_req.l4_rules, list) {
-			err = NULL;
-			if (arule->check_ptr && !arule->check_ptr(arule, curproxy, &err)) {
-				ha_alert("Proxy '%s': %s.\n", curproxy->id, err);
-				free(err);
-				cfgerr++;
-			}
-		}
-
-		/* check validity for 'tcp-request' layer 5 rules */
-		list_for_each_entry(arule, &curproxy->tcp_req.l5_rules, list) {
-			err = NULL;
-			if (arule->check_ptr && !arule->check_ptr(arule, curproxy, &err)) {
-				ha_alert("Proxy '%s': %s.\n", curproxy->id, err);
-				free(err);
-				cfgerr++;
-			}
-		}
-
-		/* check validity for 'tcp-request' layer 6 rules */
-		list_for_each_entry(arule, &curproxy->tcp_req.inspect_rules, list) {
-			err = NULL;
-			if (arule->check_ptr && !arule->check_ptr(arule, curproxy, &err)) {
-				ha_alert("Proxy '%s': %s.\n", curproxy->id, err);
-				free(err);
-				cfgerr++;
-			}
-		}
-
-		/* check validity for 'http-request' layer 7 rules */
-		list_for_each_entry(arule, &curproxy->http_req_rules, list) {
-			err = NULL;
-			if (arule->check_ptr && !arule->check_ptr(arule, curproxy, &err)) {
-				ha_alert("Proxy '%s': %s.\n", curproxy->id, err);
-				free(err);
-				cfgerr++;
-			}
-		}
-
-		/* check validity for 'http-response' layer 7 rules */
-		list_for_each_entry(arule, &curproxy->http_res_rules, list) {
-			err = NULL;
-			if (arule->check_ptr && !arule->check_ptr(arule, curproxy, &err)) {
-				ha_alert("Proxy '%s': %s.\n", curproxy->id, err);
-				free(err);
-				cfgerr++;
-			}
-		}
-
-		/* move any "block" rules at the beginning of the http-request rules */
-		if (!LIST_ISEMPTY(&curproxy->block_rules)) {
-			/* insert block_rules into http_req_rules at the beginning */
-			curproxy->block_rules.p->n    = curproxy->http_req_rules.n;
-			curproxy->http_req_rules.n->p = curproxy->block_rules.p;
-			curproxy->block_rules.n->p    = &curproxy->http_req_rules;
-			curproxy->http_req_rules.n    = curproxy->block_rules.n;
-			LIST_INIT(&curproxy->block_rules);
-		}
+		/* check validity for 'tcp-request' layer 4/5/6/7 rules */
+		cfgerr += check_action_rules(&curproxy->tcp_req.l4_rules, curproxy, &err_code);
+		cfgerr += check_action_rules(&curproxy->tcp_req.l5_rules, curproxy, &err_code);
+		cfgerr += check_action_rules(&curproxy->tcp_req.inspect_rules, curproxy, &err_code);
+		cfgerr += check_action_rules(&curproxy->tcp_rep.inspect_rules, curproxy, &err_code);
+		cfgerr += check_action_rules(&curproxy->http_req_rules, curproxy, &err_code);
+		cfgerr += check_action_rules(&curproxy->http_res_rules, curproxy, &err_code);
 
 		if (curproxy->table && curproxy->table->peers.name) {
 			struct peers *curpeers;

include/proto/action.h
@@ -72,6 +72,11 @@ static inline void action_build_list(struct list *keywords,
 		*p = '\0';
 }
 
+/* Check an action ruleset validity. It returns the number of error encountered
+ * andd err_code is updated if a warning is emitted.
+ */
+int check_action_rules(struct list *rules, struct proxy *px, int *err_code);
+
 /* for an action ACT_ACTION_TRK_SC*, return a tracking index starting at zero
  * for SC0. Unknown actions also return zero.
  */

src/action.c
@@ -16,11 +16,34 @@
 #include <common/standard.h>
 
 #include <proto/action.h>
+#include <proto/log.h>
 #include <proto/obj_type.h>
 #include <proto/proxy.h>
 #include <proto/stick_table.h>
 #include <proto/task.h>
 
+
+/* Check an action ruleset validity. It returns the number of error encountered
+ * andd err_code is updated if a warning is emitted.
+ */
+int check_action_rules(struct list *rules, struct proxy *px, int *err_code)
+{
+	struct act_rule *rule;
+	char *errmsg = NULL;
+	int err = 0;
+
+	list_for_each_entry(rule, rules, list) {
+		if (rule->check_ptr && !rule->check_ptr(rule, px, &errmsg)) {
+			ha_alert("Proxy '%s': %s.\n", px->id, errmsg);
+			err++;
+		}
+
+		free(errmsg);
+		errmsg = NULL;
+	}
+
+	return err;
+}
 
 /* Find and check the target table used by an action ACT_ACTION_TRK_*. This
  * function should be called during the configuration validity check.

commit feeccc6e40e4d61b1e9fa84aca961aceabe39371

MEDIUM: actions: Fix block ACL.
Commit 8ab2a364a8c8adf0965e74a41a2ff3cebd43e7a9 accidentally removed the code
needed for block rules while refactoring the function, so bring it back.
This is a direct commit to 2.0, as block rules were deprecated, and removed
in 2.1
The issue was reported, and the patch provided, by Bart van der Schans.

src/cfgparse.c
@@ -2831,6 +2831,16 @@ int check_config_validity()
 		cfgerr += check_action_rules(&curproxy->http_req_rules, curproxy, &err_code);
 		cfgerr += check_action_rules(&curproxy->http_res_rules, curproxy, &err_code);
 
+		/* move any "block" rules at the beginning of the http-request rules */
+		if (!LIST_ISEMPTY(&curproxy->block_rules)) {
+			/* insert block_rules into http_req_rules at the beginning */
+			curproxy->block_rules.p->n    = curproxy->http_req_rules.n;
+			curproxy->http_req_rules.n->p = curproxy->block_rules.p;
+			curproxy->block_rules.n->p    = &curproxy->http_req_rules;
+			curproxy->http_req_rules.n    = curproxy->block_rules.n;
+			LIST_INIT(&curproxy->block_rules);
+		}
+
 		if (curproxy->table && curproxy->table->peers.name) {
 			struct peers *curpeers;
 