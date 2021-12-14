4cfdf47c50a84c898857620aa10348913ea8a7a4
William Lallemand <wlallemand@haproxy.org>
2021-12-14 15:22:29 +0100

    MINOR: cli: "show version" displays the current process version
    This patch implements a simple "show version" command which returns
    the version of the current process.
    
    It's available from the master and the worker processes, so it is easy
    to check if the master and the workers have the same version.
    
    This is a minor patch that really improve compatibility checks
    for scripts.
    
    Could be backported in haproxy version as far as 2.0.
    
    (cherry picked from commit 740629e2966641871b183d1c728fa7aeda45a1cc)
    Signed-off-by: William Lallemand <wlallemand@haproxy.org>
    (cherry picked from commit 02c2cc8cbec61287af9af54b7b712fdb5f9747ca)
    Signed-off-by: William Lallemand <wlallemand@haproxy.org>
    (cherry picked from commit 7ceae1189a8dce334c3078be0f9dffdb99f90c10)
    [wla: context changes in cli.c]
    Signed-off-by: William Lallemand <wlallemand@haproxy.org>
    (cherry picked from commit ed4f13530956b94b8fc4cf22b9655c8189fa34d8)
    Signed-off-by: William Lallemand <wlallemand@haproxy.org>
    (cherry picked from commit e707952fd117514900d66bd32770db49e92503d7)
    [wla: cli_dynmsg() does not exist in 2.0, replace it by a memprintf in a
    static storage]
    Signed-off-by: William Lallemand <wlallemand@haproxy.org>

--- src/cli.c
+++ src/cli.c
@@ -1517,6 +1517,19 @@ static int cli_parse_set_lvl(char **args, char *payload, struct appctx *appctx, 
 	return 1;
 }
 
+/* shows HAProxy version */
+static int cli_parse_show_version(char **args, char *payload, struct appctx *appctx, void *private)
+{
+	static char *msg = NULL;
+
+	if (!msg)
+		memprintf(&msg, "%s\n", haproxy_version);
+
+	appctx->ctx.cli.severity = LOG_INFO;
+	appctx->ctx.cli.msg = msg;
+	appctx->st0 = CLI_ST_PRINT;
+	return 1;
+}
 
 int cli_parse_default(char **args, char *payload, struct appctx *appctx, void *private)
 {
@@ -2826,6 +2839,7 @@ static struct cli_kw_list cli_kws = {{ },{
 	{ { "show", "cli", "level", NULL },    "show cli level   : display the level of the current CLI session", cli_parse_show_lvl, NULL, NULL, NULL, ACCESS_MASTER},
 	{ { "show", "fd", NULL }, "show fd [num] : dump list of file descriptors in use", cli_parse_show_fd, cli_io_handler_show_fd, NULL },
 	{ { "show", "activity", NULL }, "show activity : show per-thread activity stats (for support/developers)", cli_parse_default, cli_io_handler_show_activity, NULL },
+	{ { "show", "version", NULL },  "show version  : show version of the current process", cli_parse_show_version, NULL, NULL, NULL, ACCESS_MASTER },
 	{ { "operator", NULL },  "operator       : lower the level of the current CLI session to operator", cli_parse_set_lvl, NULL, NULL, NULL, ACCESS_MASTER},
 	{ { "user", NULL },      "user           : lower the level of the current CLI session to user", cli_parse_set_lvl, NULL, NULL, NULL, ACCESS_MASTER},
 	{ { "_getsocks", NULL }, NULL,  _getsocks, NULL },
