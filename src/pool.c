889da687a09579bc78d9978928ebb96cc02cff01
Willy Tarreau <w@1wt.eu>
2022-02-09 16:33:22 +0100

    DEBUG: pools: let's add reverse mapping from cache heads to thread and pool
    During global eviction we're visiting nodes from the LRU tail and we
    determine their pool cache head and their pool. In order to make sure
    we never mess up, let's add some backwards pointer to the thread number
    and pool from the pool_cache_head. It's 64-byte aligned anyway so we're
    not wasting space and it helps for debugging and will prevent memory
    corruption the earliest possible.
    
    (cherry picked from commit 49bb5d4268adaea64490ee20f4d73d94afe6a903)
    [wt: context adjustment]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/pool.c
+++ src/pool.c
@@ -190,6 +190,8 @@ struct pool_head *create_pool(char *name, unsigned int size, unsigned int flags)
 		/* update per-thread pool cache if necessary */
 		for (thr = 0; thr < MAX_THREADS; thr++) {
 			LIST_INIT(&pool->cache[thr].list);
+			pool->cache[thr].tid = thr;
+			pool->cache[thr].pool = pool;
 		}
 #endif
 	}
@@ -311,7 +313,11 @@ void pool_evict_from_local_caches()
 		 * oldest in their own pools, thus their next is the pool's head.
 		 */
 		ph = LIST_NEXT(&item->by_pool, struct pool_cache_head *, list);
+		BUG_ON(ph->tid != tid);
+
 		pool = container_of(ph - tid, struct pool_head, cache);
+		BUG_ON(pool != ph->pool);
+
 		pool_check_pattern(ph, item, pool->size);
 		LIST_DELETE(&item->by_pool);
 		LIST_DELETE(&item->by_lru);
