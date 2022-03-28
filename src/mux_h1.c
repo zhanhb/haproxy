79e49a102acf765ea70b3a702f20e06c8510c220
Christopher Faulet <cfaulet@haproxy.com>
2022-03-28 16:19:02 +0200

    BUG/MEDIUM: mux-h1: Properly detect full buffer cases during message parsing
    When the destination buffer is full while there are still data to parse, the
    h1s must be marked as congested to be able to restart the parsing
    later. This work on headers and data parsing. But on trailers parsing, we
    fail to do so when the buffer is full before to parse the trailers. In this
    case, we skip the trailers parsing but the h1s is not marked as
    congested. This is important to be sure to wake up the mux to restart the
    parsing when some room is made in the buffer.
    
    Because of this bug, the message processing may hang till a timeout is
    triggered. Note that for 2.3 and 2.2, the EOM processing is buggy too, for
    the same reason. It should be fixed too on these versions. On the 2.0, only
    trailers parsing is affected.
    
    This patch must be backported as far as 2.0. On 2.3 and 2.2, the EOM parsing
    must be fixed too.
    
    (cherry picked from commit 744451c7c412677dadfe626642ea5d980208e0d1)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 7e34a1824386e653fecdaae2e8db42189382cf57)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit a1d43d1fad324b53b75887216e6c44be357c8bf3)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit f5e1fb2514fa24f85d41daa2f016651c4c4943a0)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/mux_h1.c
+++ src/mux_h1.c
@@ -1385,7 +1385,7 @@ static size_t h1_process_trailers(struct h1s *h1s, struct h1m *h1m, struct htx *
 			TRACE_STATE("parsing error", H1_EV_RX_DATA|H1_EV_RX_TLRS|H1_EV_H1S_ERR, h1s->h1c->conn, h1s);
 			h1_capture_bad_message(h1s->h1c, h1s, h1m, buf);
 		}
-		else if (ret == -2) {
+		else if (ret == -2 || b_data(buf) != *ofs) {
 			TRACE_STATE("RX path congested, waiting for more space", H1_EV_RX_DATA|H1_EV_RX_TLRS|H1_EV_H1S_BLK, h1s->h1c->conn, h1s);
 			h1s->flags |= H1S_F_RX_CONGESTED;
 		}
