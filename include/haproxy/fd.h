425542423ce1a197bc11eb527e406bac1784fb87
William Lallemand <wlallemand@haproxy.org>
2022-07-26 10:35:24 +0200

    DEBUG: fd: split the fd check
    Split the BUG_ON(fd < 0 || fd >= global.maxsock) so it's easier to know
    if it quits because of a -1.
    
    (cherry picked from commit dc66f2f97d76613d732c49cc326791863b018f0e)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- include/haproxy/fd.h
+++ include/haproxy/fd.h
@@ -330,7 +330,8 @@ static inline void fd_insert(int fd, void *owner, void (*iocb)(int fd), unsigned
 	/* This must never happen and would definitely indicate a bug, in
 	 * addition to overwriting some unexpected memory areas.
 	 */
-	BUG_ON(fd < 0 || fd >= global.maxsock);
+	BUG_ON(fd < 0);
+	BUG_ON(fd >= global.maxsock);
 	BUG_ON(fdtab[fd].owner != NULL);
 	BUG_ON(fdtab[fd].state != 0);
 
