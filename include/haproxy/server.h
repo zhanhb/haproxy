29962f700084fc7b1aef6bbdb28a1c2387a418c2
Christopher Faulet <cfaulet@haproxy.com>
2022-08-03 11:28:08 +0200

    REORG: server: Export srv_settings_cpy() function
    This function will be used to init a proxy with settings of the default
    proxy. It is mandatory to fix a bug. To do so, it must be exposed.
    
    (cherry picked from commit b32cb9b5151d4b42ff92edd707d61960f8b3a536)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit b45110ad060b4e980d6485ed686ab9418debad81)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 582688ac13ed14dfd0369005920d73d7c351189d)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- include/haproxy/server.h
+++ include/haproxy/server.h
@@ -45,6 +45,7 @@ extern struct dict server_key_dict;
 int srv_downtime(const struct server *s);
 int srv_lastsession(const struct server *s);
 int srv_getinter(const struct check *check);
+void srv_settings_cpy(struct server *srv, const struct server *src, int srv_tmpl);
 int parse_server(const char *file, int linenum, char **args, struct proxy *curproxy, const struct proxy *defproxy, int parse_flags);
 int srv_update_addr(struct server *s, void *ip, int ip_sin_family, const char *updater);
 const char *srv_update_addr_port(struct server *s, const char *addr, const char *port, char *updater);
