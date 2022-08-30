d4b200f44a5435407371cfa1e1f9dbf44739d4e2
William Lallemand <wlallemand@haproxy.org>
2022-08-30 17:32:38 +0200

    BUG/MINOR: ssl: revert two wrong fixes with ckhi_link
    This reverts commit 056ad01d55675ab2d65c7b41a2e1096db27b3d14.
    This reverts commit ddd480cbdc0d54b3426ce9b6dd68cd849747cb07.
    
    The architecture is ambiguous here: ckch_inst_free() is detaching and
    freeing the "ckch_inst_link" linked list which must be free'd only from
    the cafile_entry side.
    
    The problem was also hidden by the fix ddd480c ("BUG/MEDIUM: ssl: Fix a
    UAF when old ckch instances are released") which change the ckchi_link
    inner loop by a safe one. However this can't fix entirely the problem
    since both __ckch_inst_free_locked() could remove several nodes in the
    ckchi_link linked list.
    
    This revert is voluntary reintroducing a memory leak before really fixing
    the problem.
    
    Must be backported in 2.5 + 2.6.
    
    (cherry picked from commit 0bfa3e7ff28942b374a937f5d307b1007b076545)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit d95e4a551f8b682becf5e71841c8a10185b0637d)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/ssl_ckch.c
+++ src/ssl_ckch.c
@@ -929,7 +929,6 @@ void ckch_inst_free(struct ckch_inst *inst)
 	list_for_each_entry_safe(link_ref, link_ref_s, &inst->cafile_link_refs, list) {
 		LIST_DELETE(&link_ref->link->list);
 		LIST_DELETE(&link_ref->list);
-		free(link_ref->link);
 		free(link_ref);
 	}
 
@@ -2556,7 +2555,7 @@ static int cli_io_handler_commit_cafile_crlfile(struct appctx *appctx)
 	struct stream_interface *si = appctx->owner;
 	int y = 0;
 	struct cafile_entry *old_cafile_entry = NULL, *new_cafile_entry = NULL;
-	struct ckch_inst_link *ckchi_link, *ckchi_link_back;
+	struct ckch_inst_link *ckchi_link;
 
 	if (unlikely(si_ic(si)->flags & (CF_WRITE_ERROR|CF_SHUTW)))
 		goto end;
@@ -2665,7 +2664,7 @@ static int cli_io_handler_commit_cafile_crlfile(struct appctx *appctx)
 				}
 
 				/* delete the old sni_ctx, the old ckch_insts and the ckch_store */
-				list_for_each_entry_safe(ckchi_link, ckchi_link_back, &old_cafile_entry->ckch_inst_link, list) {
+				list_for_each_entry(ckchi_link, &old_cafile_entry->ckch_inst_link, list) {
 					__ckch_inst_free_locked(ckchi_link->ckch_inst);
 				}
 
