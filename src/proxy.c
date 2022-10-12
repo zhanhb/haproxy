b956eb0dbd8e0ee582ae2b9042af8dc4fddab884
Willy Tarreau <w@1wt.eu>
2022-10-12 21:40:31 +0200

    BUG/MINOR: server: make sure "show servers state" hides private bits
    In the past we've seen "show servers state" dump some internal bits for
    the check states, that were causing regtests to fail. The relevant bits
    have been added to the doc to fix the public API and make sure they do
    not change by accident, but the output doesn't take care of masking the
    undesired ones, causing regtests (and possibly user programs) to fail
    when new bits are added. Let's add the mask for the only documented ones
    (0x0F for check and 0x1F for agent respectively).
    
    This could be backported wherever the server state is present, though
    there's a tiny risk that some undocumented bits might have already
    leaked to some user scripts, so it might be wise to wait a bit before
    doing that or even not to backport too far.
    
    (cherry picked from commit 99521abd59a255538f2f9a64d3379c31aef5a630)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 2f824716d30f659c847e1a00932a8b0eaa54ad4b)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 6a6f55e9d622fb8d639775093a42d90465eef559)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/proxy.c
+++ src/proxy.c
@@ -2556,7 +2556,7 @@ static int dump_servers_state(struct stream_interface *si)
 			             px->uuid, px->id,
 			             srv->puid, srv->id, srv_addr,
 			             srv->cur_state, srv->cur_admin, srv->uweight, srv->iweight, (long int)srv_time_since_last_change,
-			             srv->check.status, srv->check.result, srv->check.health, srv->check.state, srv->agent.state,
+			             srv->check.status, srv->check.result, srv->check.health, srv->check.state & 0x0F, srv->agent.state & 0x1F,
 			             bk_f_forced_id, srv_f_forced_id, srv->hostname ? srv->hostname : "-", srv->svc_port,
 			             srvrecord ? srvrecord : "-", srv->use_ssl, srv->check.port,
 				     srv_check_addr, srv_agent_addr, srv->agent.port);
