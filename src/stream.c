13f43a7a73c60ece27684ff3e9cc2ec02b7338f1
Christopher Faulet <cfaulet@haproxy.com>
2022-10-25 16:28:13 +0200

    BUG/MINOR: stick-table: Use server_id instead of std_t_sint in process_store_rules()
    Te commit 8c3cdf0a99 ("BUG/MAJOR: stick-tables: do not try to index a server
    name for applets") was not properly backported. On HAProxy 2.4, the server
    id have to be store in the server_id data type.
    
    It is a 2.4-specific commit. Thus there is no upstream ID. No backport
    needed.

--- src/stream.c
+++ src/stream.c
@@ -1457,7 +1457,7 @@ static int process_store_rules(struct stream *s, struct channel *rep, int an_bit
 
 		HA_RWLOCK_WRLOCK(STK_SESS_LOCK, &ts->lock);
 		ptr = __stktable_data_ptr(t, ts, STKTABLE_DT_SERVER_ID);
-		stktable_data_cast(ptr, std_t_sint) = __objt_server(s->target)->puid;
+		stktable_data_cast(ptr, server_id) = __objt_server(s->target)->puid;
 
 		if (key) {
 			de = dict_insert(&server_key_dict, key);
