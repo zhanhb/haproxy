07007514f84adf654f316ad95c6b5b3b46263c07
Willy Tarreau <w@1wt.eu>
2022-11-17 11:08:03 +0100

    BUG/MINOR: pool/cli: use ullong to report total pool usage in bytes
    As noticed by Gabriel Tzagkarakis in issue #1903, the total pool size
    in bytes is historically still in 32 bits, but at least we should report
    the product of the number of objects and their size in 64 bits so that
    the value doesn't wrap around 4G.
    
    This may be backported to all versions.
    
    (cherry picked from commit 0c5e9896c79cbec3507894ed33a8553127bc170e)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit c65d18df9ec531576cd88185ee60cdf6486cc17a)
    [cf: ctx adjustment]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/pool.c
+++ src/pool.c
@@ -552,10 +552,10 @@ void pool_destroy_all()
 void dump_pools_to_trash()
 {
 	struct pool_head *entry;
-	unsigned long allocated, used;
+	unsigned long long allocated, used;
 	int nbpools;
 #ifdef CONFIG_HAP_POOLS
-	unsigned long cached_bytes = 0;
+	unsigned long long cached_bytes = 0;
 	uint cached = 0;
 #endif
 
@@ -566,15 +566,15 @@ void dump_pools_to_trash()
 		int i;
 		for (cached = i = 0; i < global.nbthread; i++)
 			cached += entry->cache[i].count;
-		cached_bytes += cached * entry->size;
+		cached_bytes += cached * (ullong)entry->size;
 #endif
-		chunk_appendf(&trash, "  - Pool %s (%u bytes) : %u allocated (%u bytes), %u used"
+		chunk_appendf(&trash, "  - Pool %s (%u bytes) : %u allocated (%llu bytes), %u used"
 #ifdef CONFIG_HAP_POOLS
 			      " (~%u by thread caches)"
 #endif
 			      ", needed_avg %u, %u failures, %u users, @%p%s\n",
 		              entry->name, entry->size, entry->allocated,
-		              entry->size * entry->allocated, entry->used,
+		              (ullong)entry->size * entry->allocated, entry->used,
 #ifdef CONFIG_HAP_POOLS
 		              cached,
 #endif
@@ -582,13 +582,13 @@ void dump_pools_to_trash()
 		              entry->users, entry,
 		              (entry->flags & MEM_F_SHARED) ? " [SHARED]" : "");
 
-		allocated += entry->allocated * entry->size;
-		used += entry->used * entry->size;
+		allocated += entry->allocated * (ullong)entry->size;
+		used += entry->used * (ullong)entry->size;
 		nbpools++;
 	}
-	chunk_appendf(&trash, "Total: %d pools, %lu bytes allocated, %lu used"
+	chunk_appendf(&trash, "Total: %d pools, %llu bytes allocated, %llu used"
 #ifdef CONFIG_HAP_POOLS
-		      " (~%lu by thread caches)"
+		      " (~%llu by thread caches)"
 #endif
 		      ".\n",
 	              nbpools, allocated, used
