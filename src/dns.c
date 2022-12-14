cb32074e0f60dfc7b79222e5616df7193bb94336
Christopher Faulet <cfaulet@haproxy.com>
2022-12-14 10:26:25 +0100

    BUG/MEDIUM: resolvers: Use tick_first() to update the resolvers task timeout
    In resolv_update_resolvers_timeout(), the resolvers task timeout is updated
    by checking running and waiting resolutions. However, to find the next
    wakeup date, MIN() operator is used to compare ticks. Ticks must never be
    compared with such operators, tick helper functions must be used, to
    properly handled TICK_ETERNITY value. In this case, tick_first() must be
    used instead of MIN().
    
    It is an old bug but it is pretty visible since the commit fdecaf6ae4
    ("BUG/MINOR: resolvers: do not run the timeout task when there's no
    resolution"). Because of this bug, the resolvers task timeout may be set to
    TICK_ETERNITY, stopping periodic resolutions.
    
    This patch should solve the issue #1962. It must be backported to all stable
    versions.
    
    (cherry picked from commit 819d48b14e7edcebc737e5fc7d4f1b269c07bc5d)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit d94ca04f965fd5a2ad7ee500b8bbf46acd722206)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit cba3678307dec299af8649bde3bf032da86c575b)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 184a9ffead932328b7e12a03d545b69f2f21562e)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 1039c3352d65088826e7d1b9165b7b9f766dded7)
    [cf: applied in src/dns.c]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 3e8cc2e1bb65e6e44d69b47513ceea607b0c2688)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/dns.c
+++ src/dns.c
@@ -175,11 +175,11 @@ static void dns_update_resolvers_timeout(struct dns_resolvers *resolvers)
 	next = tick_add(now_ms, resolvers->timeout.resolve);
 	if (!LIST_ISEMPTY(&resolvers->resolutions.curr)) {
 		res  = LIST_NEXT(&resolvers->resolutions.curr, struct dns_resolution *, list);
-		next = MIN(next, tick_add(res->last_query, resolvers->timeout.retry));
+		next = tick_first(next, tick_add(res->last_query, resolvers->timeout.retry));
 	}
 
 	list_for_each_entry(res, &resolvers->resolutions.wait, list)
-		next = MIN(next, tick_add(res->last_resolution, dns_resolution_timeout(res)));
+		next = tick_first(next, tick_add(res->last_resolution, dns_resolution_timeout(res)));
 
 	resolvers->t->expire = next;
 	task_queue(resolvers->t);
