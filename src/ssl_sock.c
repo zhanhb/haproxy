9e63d2a10ad1807eaca6cdbc912893b1fd61a049
Frédéric Lécaille <flecaille@haproxy.com>
2023-02-01 17:56:57 +0100

    MEDIUM: quic: Remove qc_conn_finalize() from the ClientHello TLS callbacks
    This is a bad idea to make the TLS ClientHello callback call qc_conn_finalize().
    If this latter fails, this would generate a TLS alert and make the connection
    send packet whereas it is not functional. But qc_conn_finalize() job was to
    install the transport parameters sent by the QUIC listener. This installation
    cannot be done at any time. This must be done after having possibly negotiated
    the QUIC version and before sending the first Handshake packets. It seems
    the better moment to do that in when the Handshake TX secrets are derived. This
    has been found inspecting the ngtcp2 code. Calling SSL_set_quic_transport_params()
    too late would make the ServerHello to be sent without the transport parameters.
    
    The code for the connection update which was done from qc_conn_finalize() has
    been moved to quic_transport_params_store(). So, this update is done as soon as
    possible.
    
    Add QUIC_FL_CONN_TX_TP_RECEIVED to flag the connection as having received the
    peer transport parameters. Indeed this is required when the ClientHello message
    is splitted between packets.
    
    Add QUIC_FL_CONN_FINALIZED to protect the connection from calling qc_conn_finalize()
    more than one time. This latter is called only when the connection has received
    the transport parameters and after returning from SSL_do_hanshake() which is the
    function which trigger the TLS ClientHello callback call.
    
    Remove the calls to qc_conn_finalize() from from the TLS ClientHello callbacks.
    
    Must be backported to 2.6. and 2.7.
    
    (cherry picked from commit af25a69c8b0faa57a27dd5e9a99cbbe350ee6b07)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 933a302947f615d4407bb327f8ded2dbc915f031)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/ssl_sock.c
+++ src/ssl_sock.c
@@ -2679,9 +2679,10 @@ int ssl_sock_switchctx_cbk(SSL *ssl, int *al, void *arg)
 		}
 
 		if (!quic_transport_params_store(qc, 0, extension_data,
-		                                 extension_data + extension_len) ||
-		    !qc_conn_finalize(qc, 0))
+		                                 extension_data + extension_len))
 			goto abort;
+
+		qc->flags |= QUIC_FL_CONN_TX_TP_RECEIVED;
 	}
 #endif /* USE_QUIC */
 
@@ -2975,10 +2976,10 @@ int ssl_sock_switchctx_cbk(SSL *ssl, int *al, void *priv)
 		}
 
 		if (!quic_transport_params_store(qc, 0, extension_data,
-		                                 extension_data + extension_len) ||
-		    !qc_conn_finalize(qc, 0)) {
+		                                 extension_data + extension_len))
 			return SSL_TLSEXT_ERR_NOACK;
-		}
+
+		qc->flags |= QUIC_FL_CONN_TX_TP_RECEIVED;
 	}
 #endif /* USE_QUIC */
 
