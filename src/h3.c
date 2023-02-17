da5699aca118150d5d3455396aab95057e69a216
Amaury Denoyelle <adenoyelle@haproxy.com>
2023-02-17 09:51:20 +0100

    MINOR: h3/hq-interop: handle no data in decode_qcs() with FIN set
    Properly handle a STREAM frame with no data but the FIN bit set at the
    application layer. H3 and hq-interop decode_qcs() callback have been
    adjusted to not return early in this case.
    
    If the FIN bit is accepted, a HTX EOM must be inserted for the upper
    stream layer. If the FIN is rejected because the stream cannot be
    closed, a proper CONNECTION_CLOSE error will be triggered.
    
    A new utility function qcs_http_handle_standalone_fin() has been
    implemented in the qmux_http module. This allows to simply add the HTX
    EOM on qcs HTX buffer. If the HTX buffer is empty, a EOT is first added
    to ensure it will be transmitted above.
    
    This commit will allow to properly handle FIN notify through an empty
    STREAM frame. However, it is not sufficient as currently qcc_recv() skip
    the decode_qcs() invocation when the offset is already received. This
    will be fixed in the next commit.
    
    This should be backported up to 2.6 along with the next patch.
    
    (cherry picked from commit 381d8137e31d941c9143a1dc8b5760d29f388fef)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit c07a1c32d98812326dfff05cca02f8af44123b7e)
    [ad: adjusted context : no h3 stream error level on 2.6]
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>

--- src/h3.c
+++ src/h3.c
@@ -33,6 +33,7 @@
 #include <haproxy/istbuf.h>
 #include <haproxy/mux_quic.h>
 #include <haproxy/pool.h>
+#include <haproxy/qmux_http.h>
 #include <haproxy/qpack-dec.h>
 #include <haproxy/qpack-enc.h>
 #include <haproxy/quic_conn-t.h>
@@ -824,8 +825,6 @@ static ssize_t h3_decode_qcs(struct qcs *qcs, struct buffer *b, int fin)
 	ssize_t total = 0, ret;
 
 	h3_debug_printf(stderr, "%s: STREAM ID: %lu\n", __func__, qcs->id);
-	if (!b_data(b))
-		return 0;
 
 	if (quic_stream_is_uni(qcs->id) && !(h3s->flags & H3_SF_UNI_INIT)) {
 		if ((ret = h3_init_uni_stream(h3c, qcs, b)) < 0)
@@ -853,6 +852,11 @@ static ssize_t h3_decode_qcs(struct qcs *qcs, struct buffer *b, int fin)
 	if (h3s->type == H3S_T_CTRL && fin) {
 		qcc_emit_cc_app(qcs->qcc, H3_CLOSED_CRITICAL_STREAM, 1);
 		return -1;
+	}
+
+	if (!b_data(b) && fin && quic_stream_is_bidi(qcs->id)) {
+		qcs_http_handle_standalone_fin(qcs);
+		return 0;
 	}
 
 	while (b_data(b) && !(qcs->flags & QC_SF_DEM_FULL)) {
