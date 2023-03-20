44f7e3bbc8e994022eac2ddf358ad5bb7b602d07
Amaury Denoyelle <adenoyelle@haproxy.com>
2023-03-20 18:29:36 +0100

    BUG/MINOR: quic: wake up MUX on probing only for 01RTT
    On PTO probe timeout expiration, a probe packet must be emitted.
    quic_pto_pktns() is used to determine for which packet space the timer
    has expired. However, if MUX is already subscribed for sending, it is
    woken up without checking first if this happened for the 01RTT packet
    space.
    
    It is unsure that this is really a bug as in most cases, MUX is
    established only after Initial and Handshake packet spaces are removed.
    However, the situation is not se clear when 0-RTT is used. For this
    reason, adjust the code to explicitely check for the 01RTT packet space
    before waking up the MUX layer.
    
    This should be backported up to 2.6. Note that qc_notify_send() does not
    exists in 2.6 so it should be replaced by the explicit block checking
    (qc->subs && qc->subs->events & SUB_RETRY_SEND).
    
    (cherry picked from commit 2a19b6e564513db52a7f4524a6545a0ca815afe9)
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>
    (cherry picked from commit ba67151ee67e84052661f067d5cda084d5af19ea)
     [ad: adjusted context due to missing qc_notify_send().]
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>

--- src/quic_conn.c
+++ src/quic_conn.c
@@ -4815,44 +4815,45 @@ struct task *qc_process_timer(struct task *task, void *ctx, unsigned int state)
 
 	if (qc->path->in_flight) {
 		pktns = quic_pto_pktns(qc, qc->state >= QUIC_HS_ST_CONFIRMED, NULL);
-		if (qc->subs && qc->subs->events & SUB_RETRY_SEND) {
-			pktns->tx.pto_probe = QUIC_MAX_NB_PTO_DGRAMS;
-			tasklet_wakeup(qc->subs->tasklet);
-			qc->subs->events &= ~SUB_RETRY_SEND;
-			if (!qc->subs->events)
-				qc->subs = NULL;
+		if (pktns == &qc->pktns[QUIC_TLS_PKTNS_INITIAL]) {
+			if (qc_may_probe_ipktns(qc)) {
+				qc->flags |= QUIC_FL_CONN_RETRANS_NEEDED;
+				pktns->flags |= QUIC_FL_PKTNS_PROBE_NEEDED;
+				TRACE_STATE("needs to probe Initial packet number space", QUIC_EV_CONN_TXPKT, qc);
+			}
+			else {
+				TRACE_STATE("Cannot probe Initial packet number space", QUIC_EV_CONN_TXPKT, qc);
+			}
+			if (qc->pktns[QUIC_TLS_PKTNS_HANDSHAKE].tx.in_flight) {
+				qc->flags |= QUIC_FL_CONN_RETRANS_NEEDED;
+				qc->pktns[QUIC_TLS_PKTNS_HANDSHAKE].flags |= QUIC_FL_PKTNS_PROBE_NEEDED;
+				TRACE_STATE("needs to probe Handshake packet number space", QUIC_EV_CONN_TXPKT, qc);
+			}
 		}
-		else {
-			if (pktns == &qc->pktns[QUIC_TLS_PKTNS_INITIAL]) {
+		else if (pktns == &qc->pktns[QUIC_TLS_PKTNS_HANDSHAKE]) {
+			TRACE_STATE("needs to probe Handshake packet number space", QUIC_EV_CONN_TXPKT, qc);
+			qc->flags |= QUIC_FL_CONN_RETRANS_NEEDED;
+			pktns->flags |= QUIC_FL_PKTNS_PROBE_NEEDED;
+			if (qc->pktns[QUIC_TLS_PKTNS_INITIAL].tx.in_flight) {
 				if (qc_may_probe_ipktns(qc)) {
-					qc->flags |= QUIC_FL_CONN_RETRANS_NEEDED;
-					pktns->flags |= QUIC_FL_PKTNS_PROBE_NEEDED;
+					qc->pktns[QUIC_TLS_PKTNS_INITIAL].flags |= QUIC_FL_PKTNS_PROBE_NEEDED;
 					TRACE_STATE("needs to probe Initial packet number space", QUIC_EV_CONN_TXPKT, qc);
 				}
 				else {
 					TRACE_STATE("Cannot probe Initial packet number space", QUIC_EV_CONN_TXPKT, qc);
 				}
-				if (qc->pktns[QUIC_TLS_PKTNS_HANDSHAKE].tx.in_flight) {
-					qc->flags |= QUIC_FL_CONN_RETRANS_NEEDED;
-					qc->pktns[QUIC_TLS_PKTNS_HANDSHAKE].flags |= QUIC_FL_PKTNS_PROBE_NEEDED;
-					TRACE_STATE("needs to probe Handshake packet number space", QUIC_EV_CONN_TXPKT, qc);
-				}
 			}
-			else if (pktns == &qc->pktns[QUIC_TLS_PKTNS_HANDSHAKE]) {
-				TRACE_STATE("needs to probe Handshake packet number space", QUIC_EV_CONN_TXPKT, qc);
-				qc->flags |= QUIC_FL_CONN_RETRANS_NEEDED;
-				pktns->flags |= QUIC_FL_PKTNS_PROBE_NEEDED;
-				if (qc->pktns[QUIC_TLS_PKTNS_INITIAL].tx.in_flight) {
-					if (qc_may_probe_ipktns(qc)) {
-						qc->pktns[QUIC_TLS_PKTNS_INITIAL].flags |= QUIC_FL_PKTNS_PROBE_NEEDED;
-						TRACE_STATE("needs to probe Initial packet number space", QUIC_EV_CONN_TXPKT, qc);
-					}
-					else {
-						TRACE_STATE("Cannot probe Initial packet number space", QUIC_EV_CONN_TXPKT, qc);
-					}
-				}
+		}
+		else if (pktns == &qc->pktns[QUIC_TLS_PKTNS_01RTT]) {
+			/* Wake up upper layer if waiting to send new data. */
+			if (qc->subs && qc->subs->events & SUB_RETRY_SEND) {
+				pktns->tx.pto_probe = QUIC_MAX_NB_PTO_DGRAMS;
+				tasklet_wakeup(qc->subs->tasklet);
+				qc->subs->events &= ~SUB_RETRY_SEND;
+				if (!qc->subs->events)
+					qc->subs = NULL;
 			}
-			else if (pktns == &qc->pktns[QUIC_TLS_PKTNS_01RTT]) {
+			else {
 				TRACE_STATE("needs to probe 01RTT packet number space", QUIC_EV_CONN_TXPKT, qc);
 				qc->flags |= QUIC_FL_CONN_RETRANS_NEEDED;
 				pktns->flags |= QUIC_FL_PKTNS_PROBE_NEEDED;
