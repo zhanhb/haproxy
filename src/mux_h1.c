77fa864bb14588a43970a850f7b8f0869839955d
Christopher Faulet <cfaulet@haproxy.com>
2023-03-24 09:26:16 +0100

    BUG/MEDIUM: mux-h1: Wakeup H1C on shutw if there is no I/O subscription
    This old bug was revealed because of the commit 407210a34 ("BUG/MEDIUM:
    stconn: Don't rearm the read expiration date if EOI was reached"). But it is
    still possible to hit it if there is no server timeout. At first glance, the
    2.8 is not affected. But the fix remains valid.
    
    When a shutdown for writes if performed the H1 connection must be notified
    to be released. If it is subscribed for any I/O events, it is not an
    issue. But, if there is no subscription, no I/O event is reported to the H1
    connection and it remains alive. If the message was already fully received,
    nothing more happens.
    
    On my side, I was able to trigger the bug by freezing the session. Some
    users reported a spinning loop on process_stream(). Not sure how to trigger
    the loop. To freeze the session, the client timeout must be reached while
    the server response was already fully received. On old version (< 2.6), it
    only happens if there is no server timeout.
    
    To fix the issue, we must wake up the H1 connection on shutdown for writes
    if there is no I/O subscription.
    
    This patch must be backported as far as 2.0. It should fix the issue #2090
    and #2088.
    
    (cherry picked from commit 551b89677249b2107a6e4c40a0b61d01c9da6831)
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>
    (cherry picked from commit 3c9806b04c8bfb64d8febf982b1942007c18b02e)
     [ad: adjusted context]
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>
    (cherry picked from commit 668890b8618c50632738309f85f34295677c7bf3)
    [cf: context adjustment]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit ea9b08d82c7d958a1b0bde14876d0bde0c622461)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 9b949bca23b66851c552ab9295058fea8d1177f2)
    [cf: context adjustment]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/mux_h1.c
+++ src/mux_h1.c
@@ -798,8 +798,10 @@ static void h1_release(struct h1c *h1c)
 			h1c->task = NULL;
 		}
 
-		if (h1c->wait_event.tasklet)
+		if (h1c->wait_event.tasklet) {
 			tasklet_free(h1c->wait_event.tasklet);
+			h1c->wait_event.tasklet = NULL;
+		}
 
 		h1s_destroy(h1c->h1s);
 		if (conn) {
@@ -2710,6 +2712,10 @@ static void h1_shutw_conn(struct connection *conn)
 	conn_xprt_shutw(conn);
 	conn_sock_shutw(conn, (h1c && !(h1c->flags & H1C_F_ST_SILENT_SHUT)));
 	h1c->flags = (h1c->flags & ~H1C_F_CS_SHUTW_NOW) | H1C_F_CS_SHUTDOWN;
+
+	if (h1c->wait_event.tasklet && !h1c->wait_event.events)
+		tasklet_wakeup(h1c->wait_event.tasklet);
+
 	TRACE_LEAVE(H1_EV_STRM_SHUT, conn);
 }
 
