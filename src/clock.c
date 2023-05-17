a2e5d62d58d44be3a7fc4cfc355c8ed9a11bf815
Willy Tarreau <w@1wt.eu>
2023-05-17 09:02:21 +0200

    MINOR: clock: measure the total boot time
    Some huge configs take a significant amount of time to start and this
    can cause some trouble (e.g. health checks getting delayed and grouped,
    process not responding to the CLI etc). For example, some configs might
    start fast in certain environments and slowly in other ones just due to
    the use of a wrong DNS server that delays all libc's resolutions. Let's
    first start by measuring it by keeping a copy of the most recently known
    ready date, once before calling check_config_validity() and then refine
    it when leaving this function. A last call is finally performed just
    before deciding to split between master and worker processes, and it covers
    the whole boot. It's trivial to collect and even allows to get rid of a
    call to clock_update_date() in function check_config_validity() that was
    used in hope to better schedule future events.
    
    (cherry picked from commit da4aa6905c5524a30709eb4cdf25e22fc63eb86e)
    [cf: ctx adjustment]]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 2d0bff4f0a10f017786345edc9b42b7a6ffacb34)
    [wt: ctx adjustment: no start_time in 2.6]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/clock.c
+++ src/clock.c
@@ -27,6 +27,7 @@
 #include <haproxy/tools.h>
 
 struct timeval                   start_date;      /* the process's start date in wall-clock time */
+struct timeval                   ready_date;      /* date when the process was considered ready */
 volatile ullong                  global_now;      /* common monotonic date between all threads (32:32) */
 volatile uint                    global_now_ms;   /* common monotonic date in milliseconds (may wrap) */
 
