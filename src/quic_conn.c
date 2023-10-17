21cdcd12fb608f4e226c1477f5efa63e744365ce
Willy Tarreau <w@1wt.eu>
2023-10-17 17:00:10 +0200

    BUG/MEDIUM: quic_conn: let the scheduler kill the task when needed
    The two timer handlers qc_process_timer() and qc_idle_timer_task() would
    inadvertently return NULL when they don't want to be requeued, instead
    of just returning the task itself. The effect of returning NULL for the
    scheduler is that it considers the task as freed, so it must not touch
    it anymore. As such, the TASK_F_RUNNING flag is never removed from these
    tasks, and when quic_conn_release() later tries to release these tasks
    using task_destroy(), the latter sees the RUNNING flag and just sets
    ->process to NULL, hoping that the scheduler will kill them on return,
    but there's no longer being executed so this never happens and they are
    leaked.
    
    Interestingly, this doesn't seem to happen as much when multi-queue is
    set to off, but it's likely because the tasks are being replaced and the
    first ones have already been woken up and leaked, while the latter might
    only trigger on a timeout or timer renewal.
    
    This should address github issue #2310. Thanks to @hpn0t0ad for the
    numerous traces that helped understand this sequence.
    
    This must be backported to 2.7 at least, and adapted for 2.6
    (qc_idle_timer_task must return t there).
    
    (cherry picked from commit 4070e4042a1fbe64fa820992c63af7284735e854)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit ebb226f980e48a9d15830d60075f3208d149ff71)
    [wt: note that in 2.7, the tasks are only released after the timer's
     expiration so they still show up as allocated during 30s after the
     end of the test]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/quic_conn.c
+++ src/quic_conn.c
@@ -5380,7 +5380,6 @@ struct task *qc_process_timer(struct task *task, void *ctx, unsigned int state)
 
 	if (qc->flags & (QUIC_FL_CONN_DRAINING|QUIC_FL_CONN_TO_KILL)) {
 		TRACE_PROTO("cancelled action (draining state)", QUIC_EV_CONN_PTIMER, qc);
-		task = NULL;
 		goto out;
 	}
 
@@ -5962,6 +5961,7 @@ struct task *qc_idle_timer_task(struct task *t, void *ctx, unsigned int state)
 	if (qc->mux_state != QC_MUX_READY) {
 		quic_conn_release(qc);
 		qc = NULL;
+		t = NULL;
 	}
 
 	/* TODO if the quic-conn cannot be freed because of the MUX, we may at
@@ -5973,10 +5973,6 @@ struct task *qc_idle_timer_task(struct task *t, void *ctx, unsigned int state)
 		TRACE_DEVEL("dec half open counter", QUIC_EV_CONN_IDLE_TIMER, qc);
 		HA_ATOMIC_DEC(&prx_counters->half_open_conn);
 	}
-
- leave:
-	TRACE_LEAVE(QUIC_EV_CONN_IDLE_TIMER, qc);
-	return NULL;
 
  requeue:
 	TRACE_LEAVE(QUIC_EV_CONN_IDLE_TIMER, qc);
