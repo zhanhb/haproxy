bf4c982b4f16c30652e94556e272771ce656757b
Amaury Denoyelle <adenoyelle@haproxy.com>
2024-01-29 15:15:27 +0100

    MINOR: h3: add traces for stream sending function
    Replace h3_debug_printf() by real trace for functions used by stream
    layer snd_buf callback. A new event type H3_EV_STRM_SEND is created for
    the occasion.
    
    This should be backported up to 2.6 to help investigate H3 issues on
    stable releases. Note that h3_nego_ff/h3_done_ff definition are not
    available from 2.8.
    
    (cherry picked from commit df5cf9123fff25eaa8693b97e5c219827bd45581)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 45bc09c37bae81a23b59149e95abccce16aea059)
    [wt: dropped h3_nego_ff/h3_done_ff]
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 44083acdf121dba07b36fd4ebd5f3613d8aedc3f)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit edc3a9454a45d4efbaf6cdf37a7a3dfbe2c74e19)
    [cf: ctx adjt]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/h3.c
+++ src/h3.c
@@ -67,6 +67,8 @@ static const struct trace_event h3_trace_events[] = {
 	{ .mask = H3_EV_H3S_NEW,      .name = "h3s_new",     .desc = "new H3 stream" },
 #define           H3_EV_H3S_END       (1ULL <<  8)
 	{ .mask = H3_EV_H3S_END,      .name = "h3s_end",     .desc = "H3 stream terminated" },
+#define           H3_EV_STRM_SEND     (1ULL << 12)
+	{ .mask = H3_EV_STRM_SEND,    .name = "strm_send",   .desc = "sending data for stream" },
 	{ }
 };
 
@@ -1432,7 +1434,7 @@ static size_t h3_snd_buf(struct qcs *qcs, struct htx *htx, size_t count)
 	int32_t idx;
 	int ret = 0;
 
-	h3_debug_printf(stderr, "%s\n", __func__);
+	TRACE_ENTER(H3_EV_STRM_SEND, qcs->qcc->conn, qcs);
 
 	while (count && !htx_is_empty(htx) &&
 	       !(qcs->flags & QC_SF_BLK_MROOM) && !h3c->err) {
@@ -1490,6 +1492,7 @@ static size_t h3_snd_buf(struct qcs *qcs, struct htx *htx, size_t count)
 	}
 
  out:
+	TRACE_LEAVE(H3_EV_STRM_SEND, qcs->qcc->conn, qcs);
 	return total;
 }
 
