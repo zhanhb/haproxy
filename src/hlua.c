7924e89f7c870a5a2a66c9a907f0a49383c450f6
Aurelien DARRAGON <adarragon@haproxy.com>
2024-03-04 11:06:24 +0100

    BUG/MINOR: hlua: improper lock usage in hlua_filter_callback()
    In hlua_filter_callback(), some lua stack work is performed under
    SET_SAFE_LJMP() guard which also takes care of locking the hlua context
    when needed. However, a lua_gettop() call is performed out of the guard,
    thus it is unsafe in multithreading context if the script is loaded using
    'lua-load' because in this case the main lua stack is shared between
    threads and each access to a lua stack must be performed under the lock,
    thus we move lua_gettop() call under the lock.
    
    It should be backported up to 2.6.
    
    (cherry picked from commit 51f291c795570d7da9cc230616a41d55eca93f1a)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 75be318a3cee92d7cc46fec90afcf59bcaad6c1d)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit f8b137bf09f6dfe7824ae17cf667bd4c87d1dfed)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/hlua.c
+++ src/hlua.c
@@ -11027,7 +11027,7 @@ static int hlua_filter_callback(struct stream *s, struct filter *filter, const c
 		goto end;
 
 	if (!HLUA_IS_RUNNING(flt_hlua)) {
-		int extra_idx = lua_gettop(flt_hlua->T);
+		int extra_idx;
 
 		/* The following Lua calls can fail. */
 		if (!SET_SAFE_LJMP(flt_hlua)) {
@@ -11040,6 +11040,8 @@ static int hlua_filter_callback(struct stream *s, struct filter *filter, const c
 			SEND_ERR(s->be, "Lua filter '%s': %s.\n", conf->reg->name, error);
 			goto end;
 		}
+
+		extra_idx = lua_gettop(flt_hlua->T);
 
 		/* Check stack size. */
 		if (!lua_checkstack(flt_hlua->T, 3)) {
