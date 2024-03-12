0e29a978d46eec22d9508272978ee4fa99512c50
Aurelien DARRAGON <adarragon@haproxy.com>
2024-03-12 17:05:54 +0100

    BUG/MEDIUM: hlua: streams don't support mixing lua-load with lua-load-per-thread (2nd try)
    While trying to reproduce another crash case involving lua filters
    reported by @bgrooot on GH #2467, we found out that mixing filters loaded
    from different contexts ('lua-load' vs 'lua-load-per-thread') for the same
    stream isn't supported and may even cause the process to crash.
    
    Historically, mixing lua-load and lua-load-per-threads for a stream wasn't
    supported, but this changed thanks to 0913386 ("BUG/MEDIUM: hlua: streams
    don't support mixing lua-load with lua-load-per-thread").
    
    However, the above fix didn't consider lua filters's use-case properly:
    unlike lua fetches, actions or even services, lua filters don't simply
    use the stream hlua context as a "temporary" hlua running context to
    process some hlua code. For fetches, actions.. hlua executions are
    processed sequentially, so we simply reuse the hlua context from the
    previous action/fetch to run the next one (this allows to bypass memory
    allocations and initialization, thus it increases performance), unless
    we need to run on a different hlua state-id, in which case we perform a
    reset of the hlua context.
    
    But this cannot work with filters: indeed, once registered, a filter will
    last for the whole stream duration. It means that the filter will rely
    on the stream hlua context from ->attach() to ->detach(). And here is the
    catch, if for the same stream we register 2 lua filters from different
    contexts ('lua-load' + 'lua-load-per-thread'), then we have an issue,
    because the hlua stream will be re-created each time we switch between
    runtime contexts, which means each time we switch between the filters (may
    happen for each stream processing step), and since lua filters rely on the
    stream hlua to carry context between filtering steps, this context will be
    lost upon a switch. Given that lua filters code was not designed with that
    in mind, it would confuse the code and cause unexpected behaviors ranging
    from lua errors to crashing process.
    
    So here we take another approach: instead of re-creating the stream hlua
    context each time we switch between "global" and "per-thread" runtime
    context, let's have both of them inside the stream directly as initially
    suggested by Christopher back then when talked about the original issue.
    
    For this we leverage hlua_stream_ctx_prepare() and hlua_stream_ctx_get()
    helper functions which return the proper hlua context for a given stream
    and state_id combination.
    
    As for debugging infos reported after ha_panic(), we check for both hlua
    runtime contexts to check if one of them was active when the panic occured
    (only 1 runtime ctx per stream may be active at a given time).
    
    This should be backported to all stable versions with 0913386
    ("BUG/MEDIUM: hlua: streams don't support mixing lua-load with lua-load-per-thread")
    
    This commit depends on:
     - "DEBUG: lua: precisely identify if stream is stuck inside lua or not"
       [for versions < 2.9 the ha_thread_dump_one() part should be skipped]
     - "MINOR: hlua: use accessors for stream hlua ctx"
    
    For 2.4, the filters API didn't exist. However it may be a good idea to
    backport it anyway because ->set_priv()/->get_priv() from tcp/http lua
    applets may also be affected by this bug, plus it will ease code
    maintenance. Of course, filters-related parts should be skipped in this
    case.
    
    (cherry picked from commit 07b2e84bce518cc6321e8c9014f75669993dac7c)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 7614fb1acd65f1de36b0cf72ae58b2b41aad0b86)
    [cf: No need to fix ha_thread_dump_one()]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 1a3ac40c2a1813e129ea314e20e7efd374c836db)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 0fe51b4eaf82445dac301bf3c2151aab95b69012)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/debug.c
+++ src/debug.c
@@ -262,8 +262,9 @@ void ha_task_dump(struct buffer *buf, const struct task *task, const char *pfx)
 
 #ifdef USE_LUA
 	hlua = NULL;
-	if (s && s->hlua && HLUA_IS_BUSY(s->hlua)) {
-		hlua = s->hlua;
+	if (s && ((s->hlua[0] && HLUA_IS_BUSY(s->hlua[0])) ||
+	    (s->hlua[1] && HLUA_IS_BUSY(s->hlua[1])))) {
+		hlua = (s->hlua[0] && HLUA_IS_BUSY(s->hlua[0])) ? s->hlua[0] : s->hlua[1];
 		chunk_appendf(buf, "%sCurrent executing Lua from a stream analyser -- ", pfx);
 	}
 	else if (task->process == hlua_process_task && (hlua = task->context)) {
