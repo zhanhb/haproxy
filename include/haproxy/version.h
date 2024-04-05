77da0631cd5ea298716f887304f20c06e2041603
Christopher Faulet <cfaulet@haproxy.com>
2024-04-05 20:12:29 +0200

    MINOR: version: Set the EOL of the 2.7 branch
    The end-of-life of the 2.7 branch was planned for Q1 2024. This patch makes
    it official for the next coming release.

--- include/haproxy/version.h
+++ include/haproxy/version.h
@@ -39,7 +39,7 @@
 #ifdef  CONFIG_PRODUCT_STATUS
 #define PRODUCT_STATUS    CONFIG_PRODUCT_STATUS
 #else
-#define PRODUCT_STATUS   "Status: stable branch - will stop receiving fixes around Q1 2024."
+#define PRODUCT_STATUS   "Status: End of life - please upgrade to branch 2.8."
 #endif
 
 #ifdef CONFIG_PRODUCT_URL_BUGS
