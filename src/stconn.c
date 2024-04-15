439a41b305ebe9fd6028592ecbf78c950a1a13aa
Christopher Faulet <cfaulet@haproxy.com>
2024-04-15 19:09:01 +0200

    BUG/MEDIUM: stconn: Don't forward channel data if input data must be filtered
    Once data are received and placed in a channel buffer, if it is possible,
    outgoing data are immediately forwarded. But we must take care to not do so
    if there is also pending input data and a filter registered on the
    channel. It is especially important for HTX streams because the HTX may be
    altered, especially the extra field. And it is indeed an issue with the HTTP
    compression filter and the H1 multiplexer. The wrong chunk size may be
    announced leading to an internal error.
    
    This patch should fix the issue #2530. It must be backported to all stable
    versions.
    
    (cherry picked from commit 50d8c187423d6b7e9b1083e05370885f6d12e844)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit dcc0a4030d8dad26034580447c943770cd76a6da)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/stconn.c
+++ src/stconn.c
@@ -14,6 +14,7 @@
 #include <haproxy/applet.h>
 #include <haproxy/connection.h>
 #include <haproxy/check.h>
+#include <haproxy/filters.h>
 #include <haproxy/http_ana.h>
 #include <haproxy/pipe.h>
 #include <haproxy/pool.h>
@@ -1085,6 +1086,7 @@ static void sc_notify(struct stconn *sc)
 	 */
 	if (!channel_is_empty(ic) &&
 	    sc_ep_test(sco, SE_FL_WAIT_DATA) &&
+	     (!HAS_DATA_FILTERS(__sc_strm(sc), ic) || channel_input_data(ic) == 0) &&
 	     (!(sc->flags & SC_FL_SND_EXP_MORE) || channel_full(ic, co_data(ic)) || channel_input_data(ic) == 0)) {
 		int new_len, last_len;
 
