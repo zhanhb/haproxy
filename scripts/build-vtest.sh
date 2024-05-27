d1d965ae2993964eddf6a4992fc6c6c570bce8a4
Willy Tarreau <w@1wt.eu>
2024-05-27 11:50:31 +0200

    CI: scripts: fix build of vtest regarding option -C
    On Linux, GNU make emits "w" at the beginning of the MAKEFLAGS
    variable if -C is passed, which happens since vtest d6d228bcb3.
    In fact it emits any of the command line flags without the leading
    '-' in this case. gmake doesn't do that on BSD apparently. It's
    documented under Options/Recursion in the GNU make doc. There's
    also MFLAGS that could work but it does not contain the variables
    definitions. So let's just avoid the -C that we don't really need.
    
    This needs to be backported to stable versions.
    
    (cherry picked from commit 01843c47a13f01ba899d06ac968ca8a9bf7ff974)
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>
    (cherry picked from commit c42accdb438b63b9ce75306830e418d2cacb254c)
     [ad: context adjustment]
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>
    (cherry picked from commit 678043013d1014d060d4315b0fb95b6286fac05e)
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>

--- scripts/build-vtest.sh
+++ scripts/build-vtest.sh
@@ -6,5 +6,13 @@ curl -fsSL https://github.com/vtest/VTest/archive/master.tar.gz -o VTest.tar.gz
 mkdir ../vtest
 tar xvf VTest.tar.gz -C ../vtest --strip-components=1
 # Special flags due to: https://github.com/vtest/VTest/issues/12
-make -C ../vtest FLAGS="-O2 -s -Wall"
 
+# Note: do not use "make -C ../vtest", otherwise MAKEFLAGS contains "w"
+# and fails (see Options/Recursion in GNU Make doc, it contains the list
+# of options without the leading '-').
+# MFLAGS works on BSD but misses variable definitions on GNU Make.
+# Better just avoid the -C and do the cd ourselves then.
+
+cd ../vtest
+
+make FLAGS="-O2 -s -Wall"
