zhanhb/haproxy

commit 80a0ea962dc0677010dfb45d8fa6196ec774bc53

BUG/MEDIUM: stick-tables: Fix race with peers when trashing oldest entries
It is the same that the one fixed in process_table_expire() (21447b1dd4
["BUG/MAJOR: stick-tables: fix race with peers in entry expiration"]). In
stktable_trash_oldest(), when the update lock is acquired, we must take care
to check again the ref_cnt because some peers may increment it (See commit
above for details).

This patch fixes a crash mentionned in 2552#issuecomment-2110532706. It must
be backported to 2.9.

(cherry picked from commit dfd938bad6d9b7af8ca41d7044ae6e6b787abcbc)
 [ad: major context adjustment]
Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>

src/stick_table.c
@@ -224,10 +224,12 @@ int __stktable_trash_oldest(struct stktable *t, int to_batch)
 	int max_search = to_batch * 2; // no more than 50% misses
 	int batched = 0;
 	int looped = 0;
+	int updt_locked;
 
 	eb = eb32_lookup_ge(&t->exps, now_ms - TIMER_LOOK_BACK);
 
 	while (batched < to_batch) {
+		updt_locked = 0;
 
 		if (unlikely(!eb)) {
 			/* we might have reached the end of the tree, typically because
@@ -280,15 +282,31 @@ int __stktable_trash_oldest(struct stktable *t, int to_batch)
 			continue;
 		}
 
+		/* if the entry is in the update list, we must be extremely careful
+		 * because peers can see it at any moment and start to use it. Peers
+		 * will take the table's updt_lock for reading when doing that, and
+		 * with that lock held, will grab a ref_cnt before releasing the
+		 * lock. So we must take this lock as well and check the ref_cnt.
+		 */
+		if (ts->upd.node.leaf_p) {
+			if (!updt_locked) {
+				updt_locked = 1;
+				HA_RWLOCK_WRLOCK(STK_TABLE_LOCK, &t->updt_lock);
+			}
+			/* now we're locked, new peers can't grab it anymore,
+			 * existing ones already have the ref_cnt.
+			 */
+			if (HA_ATOMIC_LOAD(&ts->ref_cnt))
+				continue;
+		}
 		/* session expired, trash it */
 		ebmb_delete(&ts->key);
-		if (ts->upd.node.leaf_p) {
-			HA_RWLOCK_WRLOCK(STK_TABLE_LOCK, &t->updt_lock);
-			eb32_delete(&ts->upd);
-			HA_RWLOCK_WRUNLOCK(STK_TABLE_LOCK, &t->updt_lock);
-		}
+		eb32_delete(&ts->upd);
 		__stksess_free(t, ts);
 		batched++;
+
+		if (updt_locked)
+			HA_RWLOCK_WRUNLOCK(STK_TABLE_LOCK, &t->updt_lock);
 	}
 
 	return batched;

commit f2c62b391015cca3ae14f8183af336724be5f690

BUG/MEDIUM: stick-tables: Fix locking of updt_lock when trashing oldest entries
A bug was introduced in commit 80a0ea962d ("BUG/MEDIUM: stick-tables: Fix
race with peers when trashing oldest entries") during the backport
process. In 3.0, there are 2 nested while loops. The update lock is acquired
in the inner loop and released when exiting for it.

But on 2.9, there is only one loop. Thus the update lock is only released at
the end of the function, if it was previously acquired. So let's fix that.

It is a 2.9-specific issue. There is no upstream ID and no backport is
needed, except if commit above is backported.

src/stick_table.c
@@ -232,13 +232,11 @@ int __stktable_trash_oldest(struct stktable *t, int to_batch)
 	int max_search = to_batch * 2; // no more than 50% misses
 	int batched = 0;
 	int looped = 0;
-	int updt_locked;
+	int updt_locked = 0;
 
 	eb = eb32_lookup_ge(&t->exps, now_ms - TIMER_LOOK_BACK);
 
 	while (batched < to_batch) {
-		updt_locked = 0;
-
 		if (unlikely(!eb)) {
 			/* we might have reached the end of the tree, typically because
 			 * <now_ms> is in the first half and we're first scanning the last
@@ -312,10 +310,10 @@ int __stktable_trash_oldest(struct stktable *t, int to_batch)
 		eb32_delete(&ts->upd);
 		__stksess_free(t, ts);
 		batched++;
-
-		if (updt_locked)
-			HA_RWLOCK_WRUNLOCK(STK_TABLE_LOCK, &t->updt_lock);
 	}
+
+	if (updt_locked)
+		HA_RWLOCK_WRUNLOCK(STK_TABLE_LOCK, &t->updt_lock);
 
 	return batched;
 }