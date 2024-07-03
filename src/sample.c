64824112b36f12a2567f58e0e8ac24099431cc0b
William Lallemand <wlallemand@haproxy.com>
2024-07-03 12:35:50 +0200

    BUG/MINOR: jwt: don't try to load files with HMAC algorithm
    When trying to use a HMAC algorithm (HS256, HS384, HS512) the
    sample_conv_jwt_verify_check() function of the converter tries to load a
    file even if it is only supposed to contain a secret instead of a path.
    
    When using lua, the check function is called at runtime so it even tries
    to load file at each call... This fixes the issue for HMAC algorithm
    but this is still a problem with the other algorithms, since we don't
    have a way of pre-loading files before the call.
    
    Another solution must be found to prevent disk IO with lua using other
    algorithms.
    
    Must be backported as far as 2.6.
    
    (cherry picked from commit 883f1bdbcec7882a2e4a257e93f92be604467319)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 639d800694dc679364ddfc563353b0464a0ee32c)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 20410db1c52a4ad19b3483070ffedd9587f5b9df)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit dbcc62035da9ab6e5c8700fe712f3829b28354cc)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/sample.c
+++ src/sample.c
@@ -3797,11 +3797,12 @@ static int sample_conv_json_query(const struct arg *args, struct sample *smp, vo
 static int sample_conv_jwt_verify_check(struct arg *args, struct sample_conv *conv,
 					const char *file, int line, char **err)
 {
+	enum jwt_alg alg;
 	vars_check_arg(&args[0], NULL);
 	vars_check_arg(&args[1], NULL);
 
 	if (args[0].type == ARGT_STR) {
-		enum jwt_alg alg = jwt_parse_alg(args[0].data.str.area, args[0].data.str.data);
+		alg = jwt_parse_alg(args[0].data.str.area, args[0].data.str.data);
 
 		switch(alg) {
 		case JWT_ALG_DEFAULT:
@@ -3820,7 +3821,16 @@ static int sample_conv_jwt_verify_check(struct arg *args, struct sample_conv *co
 	}
 
 	if (args[1].type == ARGT_STR) {
-		jwt_tree_load_cert(args[1].data.str.area, args[1].data.str.data, err);
+		switch (alg) {
+			JWS_ALG_HS256:
+			JWS_ALG_HS384:
+			JWS_ALG_HS512:
+			/* don't try to load a file with HMAC algorithms */
+				break;
+			default:
+				jwt_tree_load_cert(args[1].data.str.area, args[1].data.str.data, err);
+				break;
+		}
 	}
 
 	return 1;
