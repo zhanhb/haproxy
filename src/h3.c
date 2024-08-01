14be6613bcb1c92997d8f4e4387df8e3129edbf5
Amaury Denoyelle <adenoyelle@haproxy.com>
2024-08-01 18:20:08 +0200

    BUG/MINOR: h3: properly reject too long header responses
    When encoding HTX to HTTP/3 headers on the response path, a bunch of
    ABORT_NOW() where used when buffer room was not enough. In most cases
    this is safe as output buffer has just been allocated and so is empty at
    the start of the function. However, with a header list longer than a
    whole buffer, this would cause an unexpected crash.
    
    Fix this by removing ABORT_NOW() statement with proper error return
    path. For the moment, this would cause the whole connection to be close
    rather than the stream only. This may be further improved in the future.
    
    Also remove ABORT_NOW() when encoding frame length at the end of headers
    or trailers encoding. Buffer room is sufficient as it was already
    checked prior in the same function.
    
    This should be backported up to 2.6. Special care should be handled
    however as this code path has changed frequently :
    * for 2.9 and older, the extra following statement must be inserted
      prior each newly added goto statement :
      h3c->err = H3_INTERNAL_ERROR;
    * for 2.6, trailers support is not implemented. As such, related chunks
      should just be ignored when backporting.
    
    (cherry picked from commit 48514c118ce881e2ae20aeb1edda929663a8397d)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit b8523ffaf8c34a29e38120051355115564667c46)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/h3.c
+++ src/h3.c
@@ -1568,6 +1568,7 @@ static int h3_resp_headers_send(struct qcs *qcs, struct htx *htx)
 			hdr++;
 		}
 		else {
+			/* Unhandled HTX block type. */
 			ABORT_NOW();
 			goto err;
 		}
@@ -1584,19 +1585,21 @@ static int h3_resp_headers_send(struct qcs *qcs, struct htx *htx)
 		goto err;
 	}
 
-	/* At least 5 bytes to store frame type + length as a varint max size */
-	if (b_room(res) < 5)
-		ABORT_NOW();
+	/* Buffer allocated just now : must be enough for frame type + length as a max varint size */
+	BUG_ON(b_room(res) < 5);
 
 	b_reset(&outbuf);
 	outbuf = b_make(b_tail(res), b_contig_space(res), 0, 0);
 	/* Start the headers after frame type + length */
 	headers_buf = b_make(b_head(res) + 5, b_size(res) - 5, 0, 0);
 
-	if (qpack_encode_field_section_line(&headers_buf))
-		ABORT_NOW();
+	if (qpack_encode_field_section_line(&headers_buf)) {
+		h3c->err = H3_INTERNAL_ERROR;
+		goto err;
+	}
 	if (qpack_encode_int_status(&headers_buf, status)) {
-		TRACE_ERROR("invalid status code", H3_EV_TX_HDR, qcs->qcc->conn, qcs);
+		/* TODO handle invalid status code VS no buf space left */
+		TRACE_ERROR("error during status code encoding", H3_EV_TX_HDR, qcs->qcc->conn, qcs);
 		h3c->err = H3_INTERNAL_ERROR;
 		goto err;
 	}
@@ -1628,8 +1631,10 @@ static int h3_resp_headers_send(struct qcs *qcs, struct htx *htx)
 			list[hdr].v = ist("trailers");
 		}
 
-		if (qpack_encode_header(&headers_buf, list[hdr].n, list[hdr].v))
-			ABORT_NOW();
+		if (qpack_encode_header(&headers_buf, list[hdr].n, list[hdr].v)) {
+			h3c->err = H3_INTERNAL_ERROR;
+			goto err;
+		}
 	}
 
 	/* Now that all headers are encoded, we are certain that res buffer is
@@ -1638,8 +1643,7 @@ static int h3_resp_headers_send(struct qcs *qcs, struct htx *htx)
 	frame_length_size = quic_int_getsize(b_data(&headers_buf));
 	res->head += 4 - frame_length_size;
 	b_putchr(res, 0x01); /* h3 HEADERS frame type */
-	if (!b_quic_enc_int(res, b_data(&headers_buf), 0))
-		ABORT_NOW();
+	b_quic_enc_int(res, b_data(&headers_buf), 0);
 	b_add(res, b_data(&headers_buf));
 
 	ret = 0;
@@ -1780,8 +1784,7 @@ static int h3_resp_trailers_send(struct qcs *qcs, struct htx *htx)
 	 * big enough.
 	 */
 	b_putchr(res, 0x01); /* h3 HEADERS frame type */
-	if (!b_quic_enc_int(res, b_data(&headers_buf), 8))
-		ABORT_NOW();
+	b_quic_enc_int(res, b_data(&headers_buf), 8);
 	b_add(res, b_data(&headers_buf));
 
  end:
