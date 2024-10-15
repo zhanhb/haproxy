05658956ae53e2d027ade7f237c64d79cffc1c4a
Amaury Denoyelle <adenoyelle@haproxy.com>
2024-10-15 17:37:00 +0200

    BUG/MEDIUM: quic: support wait-for-handshake
    wait-for-handshake http-request action was completely ineffective with
    QUIC protocol. This commit implements its support for QUIC.
    
    QUIC MUX layer is extended to support wait-for-handshake. A new function
    qcc_handle_wait_for_hs() is executed during qcc_io_process(). It detects
    if MUX processing occurs after underlying QUIC handshake completion. If
    this is the case, it indicates that early data may be received. As such,
    connection is flagged with CO_FL_EARLY_SSL_HS, which is necessary to
    block stream processing on wait-for-handshake action.
    
    After this, qcc subscribs on quic_conn layer for RECV notification. This
    is used to detect QUIC handshake completion. Thus,
    qcc_handle_wait_for_hs() can be reexecuted one last time, to remove
    CO_FL_EARLY_SSL_HS and notify every streams flagged as
    SE_FL_WAIT_FOR_HS.
    
    This patch must be backported up to 2.6, after a mandatory period of
    observation. Note that it relies on the backport of the two previous
    patches :
    - MINOR: quic: notify connection layer on handshake completion
    - BUG/MINOR: stream: unblock stream on wait-for-handshake completion
    
    (cherry picked from commit 0918c41ef63964a986c627d20b8a1324de639cc2)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- include/haproxy/mux_quic-t.h
+++ include/haproxy/mux_quic-t.h
@@ -35,6 +35,7 @@ enum qcs_type {
 #define QC_CF_CONN_FULL 0x00000008 /* no stream buffers available on connection */
 #define QC_CF_APP_SHUT  0x00000010 /* Application layer shutdown done. */
 #define QC_CF_ERR_CONN  0x00000020 /* fatal error reported by transport layer */
+#define QC_CF_WAIT_FOR_HS 0x00000040 /* QUIC handshake has been completed */
 
 struct qcc {
 	struct connection *conn;
