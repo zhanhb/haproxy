dbad4aea304b4f9847dba2bdd33bb8926bdba88a
Amaury Denoyelle <adenoyelle@haproxy.com>
2024-10-23 18:18:48 +0200

    BUG/MEDIUM: server: fix race on servers_list during server deletion
    Each server is inserted in a global list named servers_list on
    new_server(). This list is then only used to finalize servers
    initialization after parsing.
    
    On dynamic server creation, there is no issue as new_server() is under
    thread isolation. However, when a server is deleted after its refcount
    reached zero, srv_drop() removes it from servers_list without lock
    protection. In the longterm, this can cause list corruption and crashes,
    especially if multiple adjacent servers are removed in parallel.
    
    To fix this, convert servers_list to a mt_list. This should not impact
    performance as servers_list is not used during runtime outside of server
    creation/deletion.
    
    This should fix github issue #2733. Thanks to Chris Staite who first
    found the issue here.
    
    This must be backported up to 2.6.
    
    (cherry picked from commit 7a02fcaf20dbc19db36052bbc7001bcea3912ab5)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 38c874bad691fc74a300817d1e85f3a331d4a6b4)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit f76090f48e4fc8045c609432225de78652a4694b)
    [wt: s/MT_LIST_FOR_EACH_ENTRY_LOCKED/mt_list_for_each_entry_safe/ with
         two backup elements in 3.0; this one is good, the ones above were
         missing that fix which was mistakenly merged in the subsequent
         patch]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/server.c
+++ src/server.c
@@ -71,7 +71,7 @@ struct srv_kw_list srv_keywords = {
 __decl_thread(HA_SPINLOCK_T idle_conn_srv_lock);
 struct eb_root idle_conn_srv = EB_ROOT;
 struct task *idle_conn_task __read_mostly = NULL;
-struct list servers_list = LIST_HEAD_INIT(servers_list);
+struct mt_list servers_list = MT_LIST_HEAD_INIT(servers_list);
 
 /* SERVER DELETE(n)->ADD global tracker:
  * This is meant to provide srv->rid (revision id) value.
@@ -2485,7 +2485,7 @@ struct server *new_server(struct proxy *proxy)
 	srv->obj_type = OBJ_TYPE_SERVER;
 	srv->proxy = proxy;
 	queue_init(&srv->queue, proxy, srv);
-	LIST_APPEND(&servers_list, &srv->global_list);
+	MT_LIST_APPEND(&servers_list, &srv->global_list);
 	LIST_INIT(&srv->srv_rec_item);
 	LIST_INIT(&srv->ip_rec_item);
 	MT_LIST_INIT(&srv->prev_deleted);
@@ -2582,7 +2582,7 @@ struct server *srv_drop(struct server *srv)
 
 	HA_SPIN_DESTROY(&srv->lock);
 
-	LIST_DELETE(&srv->global_list);
+	MT_LIST_DELETE(&srv->global_list);
 	event_hdl_sub_list_destroy(&srv->e_subs);
 
 	EXTRA_COUNTERS_FREE(srv->extra_counters);
@@ -2721,7 +2721,7 @@ static int _srv_parse_tmpl_init(struct server *srv, struct proxy *px)
 		release_sample_expr(newsrv->ssl_ctx.sni);
 		free_check(&newsrv->agent);
 		free_check(&newsrv->check);
-		LIST_DELETE(&newsrv->global_list);
+		MT_LIST_DELETE(&newsrv->global_list);
 	}
 	free(newsrv);
 	return i - srv->tmpl_info.nb_low;
