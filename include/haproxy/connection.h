fa11611231916306cc48174abe49298086c5e33c
Willy Tarreau <w@1wt.eu>
2024-11-05 18:04:21 +0100

    MINOR: connection: add new sample fetch functions fc_err_name and bc_err_name
    These functions return a symbolic error code such as ECONNRESET to keep
    logs compact while making them human-readable. It's a good alternative
    to the numeric code in that it's more expressive, and a good one to the
    full message since it's shorter and more precise (some codes even match
    errno names).
    
    The doc was updated so that the symbolic names appear in the table. It
    could be useful to backport this feature to help with troubleshooting
    some issues, though backporting the doc might possibly be more annoying
    in case users have local patches already, so maybe the table update does
    not need to be backported in this case.
    
    (cherry picked from commit 601b34fe7bd50c733a437f26817580bbd56c8d56)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 2913ab11dcaac187e75e5462770ca01728d9f825)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- include/haproxy/connection.h
+++ include/haproxy/connection.h
@@ -106,6 +106,7 @@ uint64_t conn_hash_digest(char *buf, size_t bufsize,
 
 int conn_reverse(struct connection *conn);
 
+const char *conn_err_code_name(struct connection *c);
 const char *conn_err_code_str(struct connection *c);
 int xprt_add_hs(struct connection *conn);
 void register_mux_proto(struct mux_proto_list *list);
