bd480a7e0ec546f6ce2723d2689bafb13a55f5e2
Willy Tarreau <w@1wt.eu>
2024-11-06 19:29:16 +0100

    MINOR: stream: maintain a counter of the number of active streams.
    This is a simplified version of commit 18ee22ff76 ("MINOR: stream/stats:
    Expose the current number of streams in stats") that is made backportable
    to 2.9. The counter is only created but not reported to the stats. It
    will still be usable by internal functions.
    
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- include/haproxy/tinfo-t.h
+++ include/haproxy/tinfo-t.h
@@ -172,6 +172,9 @@ struct thread_ctx {
 	unsigned long long out_bytes;           /* total #of bytes emitted */
 	unsigned long long spliced_out_bytes;   /* total #of bytes emitted though a kernel pipe */
 	struct buffer *thread_dump_buffer;      /* NULL out of dump, 0x02=to alloc, valid during a dump, |0x01 once done */
+	unsigned int stream_cnt;                /* Number of streams attached to this thread */
+
+	// around 52 bytes here for shared variables
 
 	ALWAYS_ALIGN(128);
 };
