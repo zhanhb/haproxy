f6404e225e931e7831da0c00d13a66830a6842f9
Christopher Faulet <cfaulet@haproxy.com>
2024-11-18 22:37:52 +0100

    BUG/MINOR: http_ana: Report -1 for %Tr for invalid response only
    The server response time is erroneously reported as -1 when it is
    intercepted by HAProxy.
    
    As stated in the documentation, the server response time is reported as -1
    when the last response header was never seen. It happens when a server
    timeout is triggered before the server managed to process the request. It
    also happens if the response is invalid. This may be reported by the mux
    during the response parsing, but also by the HTTP analyzers. However, in
    this last case, the response time must only be reported as -1 on 502.
    
    This patch must be backported to all stable versions. It should fix the
    issue #2384.
    
    (cherry picked from commit 5863d33fce702c46b77c07d4ea82e036b11417a6)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 9464b240ed1fb593232b8c2979ef801bba41d63e)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit a40a46ecbb60b1ffd2ab01e47289119efff07b40)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit b34e5204d7ac3ac2c3a7397c0cc8eddefbe4b06a)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/http_ana.c
+++ src/http_ana.c
@@ -2086,6 +2086,7 @@ int http_process_res_common(struct stream *s, struct channel *rep, int an_bit, s
 	goto return_prx_err;
 
  return_bad_res:
+	s->logs.t_data = -1; /* was not a valid response */
 	txn->status = 502;
 	stream_inc_http_fail_ctr(s);
 	_HA_ATOMIC_INC(&s->be->be_counters.failed_resp);
@@ -2100,7 +2101,6 @@ int http_process_res_common(struct stream *s, struct channel *rep, int an_bit, s
 	/* fall through */
 
  return_prx_cond:
-	s->logs.t_data = -1; /* was not a valid response */
 	s->scb->flags |= SC_FL_NOLINGER;
 
 	if (!(s->flags & SF_ERR_MASK))
