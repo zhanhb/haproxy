481356d02251cdf5bff73428e40c71d8365174f0
Christopher Faulet <cfaulet@haproxy.com>
2024-11-20 17:20:05 +0100

    BUG/MEDIUM: h3: Properly limit the number of headers received
    The number of headers are limited before the decoding but pseudo headers and
    cookie headers consume extra slots. In practice, this lowers the maximum number
    of headers that can be received.
    
    To workaround this issue, the limit is doubled during the frame decoding to be
    sure to have enough extra slots. And the number of headers is tested against the
    configured limit after the HTX message was created to be able to report an
    error. Unfortunatly no parsing error are reported because the QUIC multiplexer
    is not able to do so for now.
    
    The same is performed on trailers to be consistent with H2.
    
    This patch should be backported as far as 2.6.
    
    (cherry picked from commit 785e63335374a6db8ef35205cdb36ea726710061)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 97f2c2e56eb8d22b8181253968733e9b80d6cbae)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 02f277a63752451021e77cf6a843978f6c193854)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/h3.c
+++ src/h3.c
@@ -458,7 +458,7 @@ static ssize_t h3_headers_to_htx(struct qcs *qcs, const struct buffer *buf,
 	struct buffer *tmp = get_trash_chunk();
 	struct htx *htx = NULL;
 	struct htx_sl *sl;
-	struct http_hdr list[global.tune.max_http_hdr];
+	struct http_hdr list[global.tune.max_http_hdr * 2];
 	unsigned int flags = HTX_SL_F_NONE;
 	struct ist meth = IST_NULL, path = IST_NULL;
 	struct ist scheme = IST_NULL, authority = IST_NULL;
@@ -813,6 +813,12 @@ static ssize_t h3_headers_to_htx(struct qcs *qcs, const struct buffer *buf,
 		}
 	}
 
+	/* Check the number of blocks agains "tune.http.maxhdr" value before adding EOH block */
+	if (htx_nbblks(htx) > global.tune.max_http_hdr) {
+		len = -1;
+		goto out;
+	}
+
 	if (!htx_add_endof(htx, HTX_BLK_EOH)) {
 		h3c->err = H3_INTERNAL_ERROR;
 		len = -1;
@@ -879,7 +885,7 @@ static ssize_t h3_trailers_to_htx(struct qcs *qcs, const struct buffer *buf,
 	struct buffer *appbuf = NULL;
 	struct htx *htx = NULL;
 	struct htx_sl *sl;
-	struct http_hdr list[global.tune.max_http_hdr];
+	struct http_hdr list[global.tune.max_http_hdr * 2];
 	int hdr_idx, ret;
 	const char *ctl;
 	int i;
@@ -989,6 +995,12 @@ static ssize_t h3_trailers_to_htx(struct qcs *qcs, const struct buffer *buf,
 		}
 
 		++hdr_idx;
+	}
+
+	/* Check the number of blocks agains "tune.http.maxhdr" value before adding EOT block */
+	if (htx_nbblks(htx) > global.tune.max_http_hdr) {
+		len = -1;
+		goto out;
 	}
 
 	if (!htx_add_endof(htx, HTX_BLK_EOT)) {
