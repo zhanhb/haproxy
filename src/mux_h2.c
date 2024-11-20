047483a1e5d2a3c7db9086d8ccc58303a7664a32
Christopher Faulet <cfaulet@haproxy.com>
2024-11-20 16:27:34 +0100

    BUG/MEDIUM: mux-h2: Check the number of headers in HEADERS frame after decoding
    There is no explicit test on the number of headers when a HEADERS frame is
    received. It is implicitely limited by the size of the header list. But it
    is twice the configured limit to be sure to decode the frame.
    
    So now, a check is performed after the HTX message was created. This way, we
    are sure to not exceed the configured limit after the decoding stage. If
    there are too many headers, a parsing error is reported.
    
    Note the same is performed on the trailers.
    
    This patch should patially address the issue #2685. It should be backported
    to all stable versions.
    
    (cherry picked from commit 63d2760dfa679bea4b7a61a1a8702af23cf26e75)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 0777b7dded26b3584a5b05fa93867224371750c5)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 13f01d65b272909409080afa83933703c9d285a7)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 9149c83f0f838e9d8eed17004f3b9f2a170aec44)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/mux_h2.c
+++ src/mux_h2.c
@@ -5247,6 +5247,7 @@ next_frame:
 	/* Trailers terminate a DATA sequence */
 	if (h2_make_htx_trailers(list, htx) <= 0) {
 		TRACE_STATE("failed to append HTX trailers into rxbuf", H2_EV_RX_FRAME|H2_EV_RX_HDR|H2_EV_H2S_ERR, h2c->conn);
+		htx->flags |= HTX_FL_PARSING_ERROR;
 		goto fail;
 	}
 	goto done;
