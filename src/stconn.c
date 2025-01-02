67f67566335b3ebdb33ba68f3baed5c4dbae8c9f
Christopher Faulet <cfaulet@haproxy.com>
2025-01-02 11:24:06 +0100

    BUG/MEDIUM: stconn: Really report blocked send if sends are blocked by an error
    A regression was introduced in commit 5ec25102ed ("BUG/MEDIUM: stconn:
    Report blocked send if sends are blocked by an error") when the patch was
    backported. Instead of checking if some outgoing data were blocked, the
    opposite was performed. The write timeout must be armed is the channel IS
    NOT empty.
    
    It should fix the issue #2754 for the 2.8. It is 2.8-specific. there is no
    upstream commit ID and no backport is needed.

--- src/stconn.c
+++ src/stconn.c
@@ -1588,7 +1588,7 @@ static int sc_conn_send(struct stconn *sc)
 		BUG_ON(sc_ep_test(sc, SE_FL_EOS|SE_FL_ERROR|SE_FL_ERR_PENDING) == (SE_FL_EOS|SE_FL_ERR_PENDING));
 		if (sc_ep_test(sc, SE_FL_ERROR))
 			sc->flags |= SC_FL_ERROR;
-		if (channel_is_empty(oc))
+		if (!channel_is_empty(oc))
 			sc_ep_report_blocked_send(sc, 0);
 		return 1;
 	}

zhanhb/haproxy

commit 5ec25102ed707f28350faf4f679451e08ce39939

BUG/MEDIUM: stconn: Report blocked send if sends are blocked by an error
When some data must be sent to the endpoint but an error was previously
reported, nothing is performed and we leave. But, in this case, the SC is not
notified the sends are blocked.

It is indeed an issue if the endpoint reports an error after consuming all
data from the SC. In the endpoint the outgoing data are trashed because of
the error, but on the SC, everything was sent, even if an error was also
reported.

Because of this bug, it is possible to have outgoing data blocked at the SC
level but without any write timeout armed. In some cases, this may lead to
blocking conditions where the stream is never closed.

So now, when outgoing data cannot be sent because an previous error was
triggered, a blocked send is reported. This way, it is possible to report a
write timeout.

This patch should fix the issue #2754. It must be backported as far as 2.8.

(cherry picked from commit fbc3de6e9e59679d2e9ece3984ce31b6a7dd418f)
Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
(cherry picked from commit f55508ab670a1b10fd4d338c1646fdaeb5ad27ad)
[cf: Only the connection part is applicable to the 2.9]
Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
(cherry picked from commit 69495df630d54ab9ae1ab0c465bd30bccae37b51)
[cf: channel_is_empty() is used to test if there are outgoing data]
Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

src/stconn.c
@@ -1588,6 +1588,8 @@ static int sc_conn_send(struct stconn *sc)
 		BUG_ON(sc_ep_test(sc, SE_FL_EOS|SE_FL_ERROR|SE_FL_ERR_PENDING) == (SE_FL_EOS|SE_FL_ERR_PENDING));
 		if (sc_ep_test(sc, SE_FL_ERROR))
 			sc->flags |= SC_FL_ERROR;
+		if (channel_is_empty(oc))
+			sc_ep_report_blocked_send(sc, 0);
 		return 1;
 	}
 

commit 67f67566335b3ebdb33ba68f3baed5c4dbae8c9f

BUG/MEDIUM: stconn: Really report blocked send if sends are blocked by an error
A regression was introduced in commit 5ec25102ed ("BUG/MEDIUM: stconn:
Report blocked send if sends are blocked by an error") when the patch was
backported. Instead of checking if some outgoing data were blocked, the
opposite was performed. The write timeout must be armed is the channel IS
NOT empty.

It should fix the issue #2754 for the 2.8. It is 2.8-specific. there is no
upstream commit ID and no backport is needed.

src/stconn.c
@@ -1588,7 +1588,7 @@ static int sc_conn_send(struct stconn *sc)
 		BUG_ON(sc_ep_test(sc, SE_FL_EOS|SE_FL_ERROR|SE_FL_ERR_PENDING) == (SE_FL_EOS|SE_FL_ERR_PENDING));
 		if (sc_ep_test(sc, SE_FL_ERROR))
 			sc->flags |= SC_FL_ERROR;
-		if (channel_is_empty(oc))
+		if (!channel_is_empty(oc))
 			sc_ep_report_blocked_send(sc, 0);
 		return 1;
 	}