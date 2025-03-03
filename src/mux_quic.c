24753e071267970793be210bf17115ff4a7e189c
Amaury Denoyelle <adenoyelle@haproxy.com>
2025-03-03 10:01:07 +0100

    MINOR: mux-quic: adapt return value of qcc_decode_qcs()
    Change return value of qcc_decode_qcs(). It now directly returns the
    value from app_ops rcv_buf callback. Function documentation is updated
    to reflect this.
    
    For now, qcc_decode_qcs() return value is ignored by callers, so this
    patch should not have any functional change. However, it will become
    necessary when implementing multiple Rx buffers per QCS, as a loop will
    be implemented to invoke qcc_decode_qcs() on several contiguous buffers.
    Decoding must be stopped however as soon as an error is returned by
    rcv_buf callback. This is also the case in case of a null value, which
    indicates there is not enough data to continue decoding.
    
    (cherry picked from commit 7b168e356f09e84f37b8a26f5c709fc33aed42cf)
    [ad: pick for next patch support. Ignore above comment on multiple QCS
     Rx buffers for this release.]
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>
    (cherry picked from commit f7d5d222a834fd813eeb9383cdc17420d2fe92c6)
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>

--- src/mux_quic.c
+++ src/mux_quic.c
@@ -888,13 +888,16 @@ static void qcs_consume(struct qcs *qcs, uint64_t bytes)
 }
 
 /* Decode the content of STREAM frames already received on the stream instance
- * <qcs>.
+ * <qcs> from the <qcc> connection.
  *
  * It is safe to remove <qcs> from <qcc> recv_list after decoding is done. Even
  * if an error is returned, caller should consider that no further Rx
  * processing can be performed for the stream, until new bytes are available.
  *
- * Returns 0 on success else non-zero.
+ * Returns the result of app_ops rcv_buf callback, which is the number of bytes
+ * successfully transcoded, or a negative error code. If no error occurred but
+ * decoding cannot proceed due to missing data, the return value is 0. The
+ * value 0 may also be returned when dealing with a standalone FIN signal.
  */
 static int qcc_decode_qcs(struct qcc *qcc, struct qcs *qcs)
 {
@@ -941,11 +944,11 @@ static int qcc_decode_qcs(struct qcc *qcc, struct qcs *qcs)
 		qcs_notify_recv(qcs);
 
 	TRACE_LEAVE(QMUX_EV_QCS_RECV, qcc->conn, qcs);
-	return 0;
+	return ret;
 
  err:
-	TRACE_LEAVE(QMUX_EV_QCS_RECV, qcc->conn, qcs);
-	return 1;
+	TRACE_DEVEL("leaving on error", QMUX_EV_QCS_RECV, qcc->conn, qcs);
+	return ret;
 }
 
 /* Register <qcs> stream for emission of STREAM, STOP_SENDING or RESET_STREAM.
