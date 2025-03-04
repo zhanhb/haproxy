e9356ede5d222319d7e88a5b888b9534b6250052
Valentine Krasnobaeva <vkrasnobaeva@haproxy.com>
2025-03-04 11:04:01 +0100

    BUG/MEIDUM: startup: return to initial cwd only after check_config_validity()
    In check_config_validity() we evaluate some sample fetch expressions
    (log-format, server rules, etc). These expressions may use external files like
    maps.
    
    If some particular 'default-path' was set in the global section before, it's no
    longer applied to resolve file pathes in check_config_validity(). parse_cfg()
    at the end of config parsing switches back to the initial cwd.
    
    This fixes the issue #2886.
    
    This patch should be backported in all stable versions since 2.4.0, including
    2.4.0.
    
    (cherry picked from commit e900ef987e4167cf0921e97b09059d757f2c0ea5)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 4d7fe11b70df072007eaa1633061e3581b9618fc)
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>
    (cherry picked from commit c8d02def1178e9df778439845e6599fb1fb30bd5)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/haproxy.c
+++ src/haproxy.c
@@ -2346,6 +2346,10 @@ static void init(int argc, char **argv)
 
 	/* Note: global.nbthread will be initialized as part of this call */
 	err_code |= check_config_validity();
+	if (*initial_cwd && chdir(initial_cwd) == -1) {
+		ha_alert("Impossible to get back to initial directory '%s' : %s\n", initial_cwd, strerror(errno));
+		exit(1);
+	}
 
 	/* update the ready date to also account for the check time */
 	clock_update_date(0, 1);
