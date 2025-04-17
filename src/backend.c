zhanhb/haproxy

commit 4de748d9a6ee2682740d706bc7016dddf101f9ea

BUG/MINOR: backend: do not overwrite srv dst address on reuse
Previously, destination address of backend connection was systematically
always reassigned. However, this step is unnecessary on connection
reuse. Indeed, reuse should only be conducted with connection using the
same destination address matching the stream requirements.

This patch removes this unnecessary assignment. It is now only performed
when reuse cannot be conducted and a new connection is instantiated.

Functionnally speaking, this patch should not change anything in theory,
as reuse is performed in conformance with the destination address.
However, it appears that it was not always properly enforced. The
systematic assignment of the destination address hides these issues, so
it is now remove. The identified bogus cases will then be fixed in the
following patches.would

This should be backported up to all stable versions.

(cherry picked from commit d7fa8e88c4cda6d115b05c65cded5acb4069ee97)
Signed-off-by: Aurelien DARRAGON <adarragon@haproxy.com>
(cherry picked from commit 00ada2c2806a007df77f0f4fa2aa85938ea0b19a)
Signed-off-by: Aurelien DARRAGON <adarragon@haproxy.com>
(cherry picked from commit 519953aa5a048bf25f4005a134a2729139e8c692)
[ada: patch manually applied because of context conflicts]
Signed-off-by: Aurelien DARRAGON <adarragon@haproxy.com>

src/backend.c
@@ -1628,6 +1628,9 @@ skip_reuse:
 			srv_conn->src = bind_addr;
 			bind_addr = NULL;
 
+			/* copy the target address into the connection */
+			*srv_conn->dst = *s->scb->dst;
+
 			if (!sockaddr_alloc(&srv_conn->dst, 0, 0)) {
 				conn_free(srv_conn);
 				return SF_ERR_RESOURCE;
@@ -1643,9 +1646,6 @@ skip_reuse:
 	/* srv_conn is still NULL only on allocation failure */
 	if (!srv_conn)
 		return SF_ERR_RESOURCE;
-
-	/* copy the target address into the connection */
-	*srv_conn->dst = *s->scb->dst;
 
 	/* Copy network namespace from client connection */
 	srv_conn->proxy_netns = cli_conn ? cli_conn->proxy_netns : NULL;

commit 550a7047e8109b22d18acf2e2eb5b09ed1f5c027

BUG/MEDIUM: backend: do not overwrite srv dst address on reuse (2)
Manual conflict resolution in 4de748d9 ("BUG/MINOR: backend: do not
overwrite srv dst address on reuse") caused the assignment to be
performed before the memory space for srv_conn->dst was allocated, which
would eventually lead to invalid writes.

Instead, the assignment must be performed after address allocation.

This patch has no upstream ID has it was written specifically for 2.8
No backport needed unless 4de748d9 was.

src/backend.c
@@ -1636,13 +1636,13 @@ skip_reuse:
 			srv_conn->src = bind_addr;
 			bind_addr = NULL;
 
-			/* copy the target address into the connection */
-			*srv_conn->dst = *s->scb->dst;
-
 			if (!sockaddr_alloc(&srv_conn->dst, 0, 0)) {
 				conn_free(srv_conn);
 				return SF_ERR_RESOURCE;
 			}
+
+			/* copy the target address into the connection */
+			*srv_conn->dst = *s->scb->dst;
 
 			srv_conn->hash_node->node.key = hash;
 		}