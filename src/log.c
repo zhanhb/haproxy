0afe09857d49561022f796b27afce98210ac6eb8
Christopher Faulet <cfaulet@haproxy.com>
2025-06-23 07:50:01 +0200

    BUG/MINOR: log: Be able to use %ID alias at anytime of the stream's evaluation
    In a log-format string, using "%[unique-id]" or "%ID" should be equivalent.
    However, for the first one, the unique ID is generated when the sample fetch
    function is called. For the alias, it is not true. It that case, the
    stream's unique ID is generated when the log message is emitted. Otherwise,
    by default, the unique id is automatically generated at the end of the HTTP
    request analysis.
    
    So, if the alias "%ID" is use in a log-format string anywhere before the end
    of the request analysis, the evaluation failed and the ID is considered as
    empty. It is not consistent and in contradiction with the "%ID"
    documentation.
    
    To fix the issue, instead of evaluating the unique ID when the log message
    is emitted, it is now performed on demand when "%ID" format is evaluated.
    
    This patch should fix the issue #3016. It should be backported to all stable
    versions. It relies on the following commit:
    
      * BUG/MINOR: stream: Avoid recursive evaluation for unique-id based on itself
    
    (cherry picked from commit 20a82027ceb7a46ce2a0cbe05e40c63d132601f7)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 609d9447e2ecfe6bc4854aa2c3ee9154a97b2710)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit b07ee7114ded568f80fb1de115e11bc355916774)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 0ecb4f4780fc0c3afce7cf15b54352fd414a0d9e)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/log.c
+++ src/log.c
@@ -3113,8 +3113,13 @@ int sess_build_logline(struct session *sess, struct stream *s, char *dst, size_t
 
 			case LOG_FMT_UNIQUEID: // %ID
 				ret = NULL;
-				if (s)
+				if (s) {
+					/* if unique-id was not generated */
+					if (!isttest(s->unique_id) && !LIST_ISEMPTY(&sess->fe->format_unique_id)) {
+						stream_generate_unique_id(s, &sess->fe->format_unique_id);
+					}
 					ret = lf_text_len(tmplog, s->unique_id.ptr, s->unique_id.len, maxsize - (tmplog - dst), tmp);
+				}
 				else
 					ret = lf_text_len(tmplog, NULL, 0, maxsize - (tmplog - dst), tmp);
 				if (ret == NULL)
@@ -3166,11 +3171,6 @@ void strm_log(struct stream *s)
 		level = LOG_INFO;
 		if (err && (sess->fe->options2 & PR_O2_LOGERRORS))
 			level = LOG_ERR;
-	}
-
-	/* if unique-id was not generated */
-	if (!isttest(s->unique_id) && !LIST_ISEMPTY(&sess->fe->format_unique_id)) {
-		stream_generate_unique_id(s, &sess->fe->format_unique_id);
 	}
 
 	if (!LIST_ISEMPTY(&sess->fe->logformat_sd)) {
