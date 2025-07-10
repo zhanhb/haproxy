0fbec0d191f01aa84fb8951a6ea7cd05c3aa2cdb
Amaury Denoyelle <adenoyelle@haproxy.com>
2025-07-10 15:27:23 +0200

    MINOR: qmux: change API for snd_buf FIN transmission
    Previous patches have fixes interim response encoding via
    h3_resp_headers_send(). However, it is still necessary to adjust h3
    layer state-machine so that several successive HTTP responses are
    accepted for a single stream.
    
    Prior to this, QMUX was responsible to decree that the final HTX message
    was encoded so that FIN stream can be emitted. However, with interim
    response, MUX is in fact unable to properly determine this. As such,
    this is the responsibility of the application protocol layer. To reflect
    this, app_ops snd_buf callback is modified so that a new output argument
    <fin> is added to it.
    
    Note that for now this commit does not bring any functional change.
    However, it will be necessary for the following patch. As such, it
    should be backported prior to it to every versions as necessary.
    
    (cherry picked from commit f349df44b4e21d8bf9b575a0aa869056a2ebaa58)
    [ad: fix context due to mising comment on h3_snd_buf]
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>
    (cherry picked from commit 96896b408d0fb0b45e5ebf59cc74a5a5c2b03dc9)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit ea3cf3af3554c31213e0b72e99e3b9bf3bd4803e)
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>

--- src/hq_interop.c
+++ src/hq_interop.c
@@ -86,7 +86,7 @@ static ssize_t hq_interop_rcv_buf(struct qcs *qcs, struct buffer *b, int fin)
 }
 
 static size_t hq_interop_snd_buf(struct qcs *qcs, struct buffer *buf,
-                                 size_t count)
+                                 size_t count, char *fin)
 {
 	enum htx_blk_type btype;
 	struct htx *htx = NULL;
@@ -97,6 +97,7 @@ static size_t hq_interop_snd_buf(struct qcs *qcs, struct buffer *buf,
 	size_t total = 0;
 	int err;
 
+	*fin = 0;
 	htx = htx_from_buf(buf);
 
 	while (count && !htx_is_empty(htx) && qcc_stream_can_send(qcs)) {
@@ -174,6 +175,8 @@ static size_t hq_interop_snd_buf(struct qcs *qcs, struct buffer *buf,
 	}
 
  end:
+	if (htx->flags & HTX_FL_EOM && htx_is_empty(htx))
+		*fin = 1;
 	htx_to_buf(htx, buf);
 
 	return total;
