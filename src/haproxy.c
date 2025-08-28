85d70e106c3430f27eaa5749f0d72fa8d6dcfa1b
William Lallemand <wlallemand@haproxy.com>
2025-08-28 15:00:19 +0200

    BUILD: mworker: fix ignoring return value of ‘read’
    Fix read return value unused result.
    
    src/haproxy.c: In function ‘main’:
    src/haproxy.c:3630:17: error: ignoring return value of ‘read’ declared with attribute ‘warn_unused_result’ [-Werror=unused-result]
     3630 |                 read(sock_pair[1], &c, 1);
          |                 ^~~~~~~~~~~~~~~~~~~~~~~~~
    
    Must be backported where d7f6819 is backported.
    
    (cherry picked from commit ffdccb6e0422723b110992db446c04b673a02981)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit b666fb4d2ddfafb6e086da67ce632da3d7d11a8b)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/haproxy.c
+++ src/haproxy.c
@@ -3767,6 +3767,7 @@ int main(int argc, char **argv)
 		int sock_pair[2];
 		char *msg = NULL;
 		char c;
+		int r __maybe_unused;
 
 		if (socketpair(PF_UNIX, SOCK_STREAM, 0, sock_pair) == -1) {
 			ha_alert("[%s.main()] Cannot create socketpair to update the new worker state\n",
@@ -3802,8 +3803,8 @@ int main(int argc, char **argv)
 		 * after confirming receipt of the "\n" from the CLI applet, so
 		 * we make sure that the fd is received correctly.
 		 */
-                shutdown(sock_pair[1], SHUT_WR);
-		read(sock_pair[1], &c, 1);
+		shutdown(sock_pair[1], SHUT_WR);
+		r = read(sock_pair[1], &c, 1);
 		close(sock_pair[1]);
 		close(sock_pair[0]);
 		ha_free(&msg);
