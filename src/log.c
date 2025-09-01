181a8a303b3bac72a2bb006b440535e16f2abda0
Alexander Stephan <alexander.stephan@sap.com>
2025-09-01 09:36:07 +0000

    BUG/MINOR: log: Add OOM checks for calloc() and malloc() in logformat parser and dup_logger()
    This patch adds missing out-of-memory (OOM) checks after calls
    to `calloc()` and `malloc()` in the logformat parser and the
    `dup_logger()` function. If memory allocation fails, an error
    is reported or NULL is returned, preventing undefined behavior
    in low-memory conditions.
    
    Co-authored-by: Christian Norbert Menges <christian.norbert.menges@sap.com>
    (cherry picked from commit 22ac1f5ee9889eb7273201de0d92449d9fc5e3fa)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 0fd91432f06ee6a7b427664be127b02c854e34ab)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit de370ed1e1925fa54778ae021f92c97f96ca1a03)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 7964b6bcc657515ee63bfc7dd07f5432d3476d46)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/log.c
+++ src/log.c
@@ -376,11 +376,11 @@ int add_to_logformat_list(char *start, char *end, int type, struct list *list_fo
 
 	if (type == LF_TEXT) { /* type text */
 		struct logformat_node *node = calloc(1, sizeof(*node));
-		if (!node) {
+		str = calloc(1, end - start + 1);
+		if (unlikely(!node || !str)) {
 			memprintf(err, "out of memory error");
 			return 0;
 		}
-		str = calloc(1, end - start + 1);
 		strncpy(str, start, end - start);
 		str[end - start] = '\0';
 		node->arg = str;
@@ -743,6 +743,8 @@ struct logsrv *dup_logsrv(struct logsrv *def)
 {
 	struct logsrv *cpy = malloc(sizeof(*cpy));
 
+	if (unlikely(!cpy))
+		return NULL;
 	/* copy everything that can be easily copied */
 	memcpy(cpy, def, sizeof(*cpy));
 
