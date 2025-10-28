62fcb2f2a4b6362b3af96a4fd0e179f48fbb5641
Huangbin Zhan <zhanhb88@gmail.com>
2025-10-28 11:39:54 +0800

    MINOR: http: fix 405,431,501 default errorfile
    A few typos were present in the default errorfiles for the status codes
    above (missing dot at the end of the sentence, extra closing bracket).
    This fixes them. This can be backported.
    
    (cherry picked from commit ad9a24ee551bc654377630da744cbb451f433012)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit cd0a5f3915cce6fc10b1fb71d4443793c2ea12cb)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 3fc6b765e9702f19b23dd2de3c0d6c6ffa78a5ff)
    [cf: HTTP-431 error message does not exist on 3.0]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/http.c
+++ src/http.c
@@ -222,11 +222,11 @@ const char *http_err_msgs[HTTP_ERR_SIZE] = {
 
 	[HTTP_ERR_405] =
 	"HTTP/1.1 405 Method Not Allowed\r\n"
-	"Content-length: 146\r\n"
+	"Content-length: 147\r\n"
 	"Cache-Control: no-cache\r\n"
 	"Content-Type: text/html\r\n"
 	"\r\n"
-	"<html><body><h1>405 Method Not Allowed</h1>\nA request was made of a resource using a request method not supported by that resource\n</body></html>\n",
+	"<html><body><h1>405 Method Not Allowed</h1>\nA request was made of a resource using a request method not supported by that resource.\n</body></html>\n",
 
 	[HTTP_ERR_407] =
 	"HTTP/1.1 407 Unauthorized\r\n"
@@ -303,11 +303,11 @@ const char *http_err_msgs[HTTP_ERR_SIZE] = {
 
 	[HTTP_ERR_501] =
 	"HTTP/1.1 501 Not Implemented\r\n"
-	"Content-length: 136\r\n"
+	"Content-length: 135\r\n"
 	"Cache-Control: no-cache\r\n"
 	"Content-Type: text/html\r\n"
 	"\r\n"
-	"<html><body><h1>501 Not Implemented</h1>\n.The server does not support the functionality required to fulfill the request.\n</body></html>\n",
+	"<html><body><h1>501 Not Implemented</h1>\nThe server does not support the functionality required to fulfill the request.\n</body></html>\n",
 
 	[HTTP_ERR_502] =
 	"HTTP/1.1 502 Bad Gateway\r\n"
