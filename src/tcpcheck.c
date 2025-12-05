139fdb7c1ad009074d03cd1c3fcc3c6456113509
Christopher Faulet <cfaulet@haproxy.com>
2025-12-05 11:04:21 +0100

    MINOR: connection/ssl: Store the SNI hash value in the connection itself
    When a SNI is set on a new connection, its hash is now saved in the
    connection itself. To do so, a dedicated field was added into the connection
    strucutre, called sni_hash. For now, this value is only used when the TLS
    session is cached.
    
    (cherry picked from commit 28654f3c9bfc1386b4e57e0f1577053dfc78f21e)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit d0c990d5fdbc98ed8446d3c3b34384fd27135286)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 3f3389a983a09859771f4f11d3c109ac0d43f150)
    [cf: ctx adjt]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 4c8f203fed5b14f4dbf886c8f730de6ca23fc28f)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/tcpcheck.c
+++ src/tcpcheck.c
@@ -1059,6 +1059,9 @@ enum tcpcheck_eval_ret tcpcheck_eval_connect(struct check *check, struct tcpchec
 	struct xprt_ops *xprt;
 	struct tcpcheck_rule *next;
 	int status, port;
+#ifdef USE_OPENSSL
+	struct ist sni = IST_NULL;
+#endif
 
 	TRACE_ENTER(CHK_EV_TCPCHK_CONN, check);
 
@@ -1200,9 +1203,14 @@ enum tcpcheck_eval_ret tcpcheck_eval_connect(struct check *check, struct tcpchec
 
 #ifdef USE_OPENSSL
 	if (connect->sni)
-		ssl_sock_set_servername(conn, connect->sni);
+		sni = ist(connect->sni);
 	else if ((connect->options & TCPCHK_OPT_DEFAULT_CONNECT) && s && s->check.sni)
-		ssl_sock_set_servername(conn, s->check.sni);
+		sni = ist(s->check.sni);
+
+	if (isttest(sni)) {
+		conn->sni_hash = ssl_sock_sni_hash(sni);
+		ssl_sock_set_servername(conn, istptr(sni));
+	}
 
 	if (connect->alpn)
 		ssl_sock_set_alpn(conn, (unsigned char *)connect->alpn, connect->alpn_len);
