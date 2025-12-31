5375d9c523e27829a73ea7649477924e16cafe22
Willy Tarreau <w@1wt.eu>
2025-12-31 12:32:21 +0100

    BUG/MINOR: backend: fix the conn_retries check for TFO
    In 2.6, the retries counter on a stream was changed from retries left
    to retries done via commit 731c8e6cf ("MINOR: stream: Simplify retries
    counter calculation"). However, one comparison fell through the cracks
    in order to detect whether or not we can use TFO (only first attempt),
    resulting in TFO never working anymore.
    
    This may be backported to all versions till 2.6.
    
    (cherry picked from commit 799653d536918a25179df1d9597dfa3c283ef989)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 2bff158e5862c2d0fc0a1f3c290308ac8c6a944b)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 3450de8f9124e71024948bf0c2b951c7a33e7f58)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit be1be6bff4a031f5da43659eb2241670d99e0167)
    [cf: ctx adjt]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/backend.c
+++ src/backend.c
@@ -1339,7 +1339,7 @@ static int do_connect_server(struct stream *s, struct connection *conn)
 
 	if (co_data(&s->res))
 		conn_flags |= CONNECT_HAS_DATA;
-	if (s->conn_retries == s->be->conn_retries)
+	if (s->conn_retries == 0)
 		conn_flags |= CONNECT_CAN_USE_TFO;
 	if (!conn_ctrl_ready(conn) || !conn_xprt_ready(conn)) {
 		ret = conn->ctrl->connect(conn, conn_flags);
