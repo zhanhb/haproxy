15217a74f7acc3987793dfcd7fc23d91de5e25ca
Christopher Faulet <cfaulet@haproxy.com>
2026-01-30 11:10:12 +0100

    BUG/MINOR: config: Check buffer pool creation for failures
    The call to init_buffer() during the worker startup may fail. In that case,
    an error message is displayed but the error was not properly handled. So
    let's add the proper check and exit on error.
    
    (cherry picked from commit 9ad9def1265c50c3a1ec8270a2c10451f38dd2c7)
    Signed-off-by: William Lallemand <wlallemand@haproxy.com>
    (cherry picked from commit 224e498ebb00ac0473a7498f637688cb29b7033e)
    Signed-off-by: William Lallemand <wlallemand@haproxy.com>
    (cherry picked from commit 895d95ff7b580d2d832b7188d0751e503978e948)
    Signed-off-by: William Lallemand <wlallemand@haproxy.com>

--- src/haproxy.c
+++ src/haproxy.c
@@ -2507,7 +2507,8 @@ static void init(int argc, char **argv)
 		deinit_and_exit(0);
 
 	/* now we know the buffer size, we can initialize the channels and buffers */
-	init_buffer();
+	if (!init_buffer())
+		exit(1); // error already reported
 
 	list_for_each_entry(pcf, &post_check_list, list) {
 		err_code |= pcf->fct();
