5206b5c9f73b420b4fd847a189a015fb8d91516f
William Lallemand <wlallemand@haproxy.com>
2026-02-12 18:48:09 +0100

    SCRIPTS: build-vtest: allow to set a TMPDIR and a DESTDIR
    Implement a way to set a destination directory using DESTDIR, and a tmp
    directory using TMPDIR.
    
    By default:
    
    - DESTDIR is ../vtest like it was done previously
    - TMPDIR  is mktemp -d
    
    Only the vtest binary is copied in DESTDIR.
    
    Example:
    
    	TMPDIR=/dev/shm/ DESTDIR=/home/user/.local/bin/ ./scripts/build-vtest.sh
    
    (cherry picked from commit f47b800ac321365e43effbf7e93efb4e2f337291)
    [wt: needed for next fix; preserved current(broken) url]
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 118a8e2bea70ddb3dbe3bebaca30634214b2509a)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 99c68a65c01b2faa58a6b298b9b2849b3d0e3273)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit d3d75aea91fe482153d5b09f593dbd4ede264c58)
    [wt: do not install vtest, in 3.0 it's run from the build dir]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- scripts/build-vtest.sh
+++ scripts/build-vtest.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+DESTDIR=${DESTDIR:-${PWD}/../vtest/}
+TMPDIR=${TMPDIR:-$(mktemp -d)}
 set -eux
 
-curl -fsSL "https://code.vinyl-cache.org/vtest/VTest2/archive/main.tar.gz" -o VTest.tar.gz
-mkdir ../vtest
-tar xvf VTest.tar.gz -C ../vtest --strip-components=1
+curl -fsSL "https://code.vinyl-cache.org/vtest/VTest2/archive/main.tar.gz" -o "${TMPDIR}/VTest.tar.gz"
+mkdir -p "${TMPDIR}/vtest"
+tar xvf ${TMPDIR}/VTest.tar.gz -C "${TMPDIR}/vtest" --strip-components=1
 # Special flags due to: https://github.com/vtest/VTest/issues/12
 
 # Note: do not use "make -C ../vtest", otherwise MAKEFLAGS contains "w"
@@ -13,6 +15,18 @@ tar xvf VTest.tar.gz -C ../vtest --strip-components=1
 # MFLAGS works on BSD but misses variable definitions on GNU Make.
 # Better just avoid the -C and do the cd ourselves then.
 
-cd ../vtest
+cd "${TMPDIR}/vtest"
 
-make FLAGS="-O2 -s -Wall"
+set +e
+CPUS=${CPUS:-$(nproc 2>/dev/null)}
+CPUS=${CPUS:-1}
+set -e
+
+#
+# temporarily detect Apple Silicon (it's using /opt/homebrew instead of /usr/local)
+#
+if test -f /opt/homebrew/include/pcre2.h; then
+   make -j${CPUS} FLAGS="-O2 -s -Wall" INCS="-I. -Isrc -Ilib -I/usr/local/include -I/opt/homebrew/include -pthread"
+else
+   make -j${CPUS} FLAGS="-O2 -s -Wall"
+fi
