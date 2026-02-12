5bfcb666f65bbfadfce0566ea3988c22c973438a
Christopher Faulet <cfaulet@haproxy.com>
2026-02-12 11:24:27 +0100

    BUG/MINOR: http-ana: Stop to wait for body on client error/abort
    During the message analysis, we must take care to stop wait for the message
    body if an error was reported on client side or an abort was detected with
    abort-on-close configured (by default now).
    
    The bug was introduced when the "wait-for-body" action was added. Only the
    producer state was tested. So, when we were waiting for the request payload,
    there was no issue. But when we were waiting for the response payload, error
    or abort on client side was not considered.
    
    This patch should be backported to all stable versions.
    
    (cherry picked from commit 0aca25f725199727718e611b392a8f4a8acdbd00)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit e1068b111d8130efb8c4fc51bbb46a19641d417d)
    [cf: proxy_abrt_close_def() does not exist on 3.2, so the corresponding flag
         is tested instead]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 325b8c256a6732ad172a4855ac7d50ad57a0f5d4)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 0f849e31112c6c064c5602114eefade9d30ed330)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/http_ana.c
+++ src/http_ana.c
@@ -4166,7 +4166,12 @@ enum rule_result http_wait_for_msg_body(struct stream *s, struct channel *chn,
 	}
 
 	/* we get here if we need to wait for more data */
-	if (!(chn_prod(chn)->flags & (SC_FL_EOS|SC_FL_ABRT_DONE))) {
+
+	if ((s->scf->flags & SC_FL_ERROR) ||
+	    ((s->scf->flags & (SC_FL_EOS|SC_FL_ABRT_DONE)) &&
+	     (s->be->options & PR_O_ABRT_CLOSE)))
+		ret = HTTP_RULE_RES_CONT;
+	else if (!(chn_prod(chn)->flags & (SC_FL_ERROR|SC_FL_EOS|SC_FL_ABRT_DONE))) {
 		if (!tick_isset(chn->analyse_exp))
 			chn->analyse_exp = tick_add_ifset(now_ms, time);
 		ret = HTTP_RULE_RES_YIELD;
