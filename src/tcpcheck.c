4cbd1d67daeb094a817c2ab110d096e5b1c8e7ea
Tim Duesterhus <tim@tideways-gmbh.com>
2026-02-20 12:20:05 +0100

    BUG/MINOR: tcpcheck: Fix typo in error error message for `http-check expect`
    With a config:
    
        backend bk_app
        	http-check expect status 200 string "status: ok"
    
    This now correctly emits the error:
    
        config : parsing [./patch.cfg:2] : 'http-check expect' : only one pattern expected.
    
    This line containing the typo is unchanged since at least HAProxy 2.2, the
    patch should be backported into all supported branches.
    
    (cherry picked from commit 760fef1fc00d1eaa18fd1f43e38d72dc2133bea7)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit deea01d9598872688416c5072b4893749a41ebb9)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 9e6b06004e16a80a7f8fe81ca62f2b4b8ded2682)
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>
    (cherry picked from commit fff2b3d8b996cf7055b2e3c6795d8af604a934d5)
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>
    (cherry picked from commit 3ce6627499d56990720bc19b1050a6024b9b2583)
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>
    (cherry picked from commit 6315b0440096436ab89eed8c386c91cce1f565fe)
    Signed-off-by: Amaury Denoyelle <adenoyelle@haproxy.com>

--- src/tcpcheck.c
+++ src/tcpcheck.c
@@ -2954,7 +2954,7 @@ struct tcpcheck_rule *parse_tcpcheck_expect(char **args, int cur_arg, struct pro
 		}
 		else if (strcmp(args[cur_arg], "string") == 0 || strcmp(args[cur_arg], "rstring") == 0) {
 			if (type != TCPCHK_EXPECT_UNDEF) {
-				memprintf(errmsg, "only on pattern expected");
+				memprintf(errmsg, "only one pattern expected");
 				goto error;
 			}
 			if (proto != TCPCHK_RULES_HTTP_CHK)
@@ -2973,7 +2973,7 @@ struct tcpcheck_rule *parse_tcpcheck_expect(char **args, int cur_arg, struct pro
 			if (proto == TCPCHK_RULES_HTTP_CHK)
 				goto bad_http_kw;
 			if (type != TCPCHK_EXPECT_UNDEF) {
-				memprintf(errmsg, "only on pattern expected");
+				memprintf(errmsg, "only one pattern expected");
 				goto error;
 			}
 			type = ((*(args[cur_arg]) == 'b') ?  TCPCHK_EXPECT_BINARY : TCPCHK_EXPECT_BINARY_REGEX);
@@ -2987,7 +2987,7 @@ struct tcpcheck_rule *parse_tcpcheck_expect(char **args, int cur_arg, struct pro
 		}
 		else if (strcmp(args[cur_arg], "string-lf") == 0 || strcmp(args[cur_arg], "binary-lf") == 0) {
 			if (type != TCPCHK_EXPECT_UNDEF) {
-				memprintf(errmsg, "only on pattern expected");
+				memprintf(errmsg, "only one pattern expected");
 				goto error;
 			}
 			if (proto != TCPCHK_RULES_HTTP_CHK)
@@ -3009,7 +3009,7 @@ struct tcpcheck_rule *parse_tcpcheck_expect(char **args, int cur_arg, struct pro
 			if (proto != TCPCHK_RULES_HTTP_CHK)
 				goto bad_tcp_kw;
 			if (type != TCPCHK_EXPECT_UNDEF) {
-				memprintf(errmsg, "only on pattern expected");
+				memprintf(errmsg, "only one pattern expected");
 				goto error;
 			}
 			type = ((*(args[cur_arg]) == 's') ? TCPCHK_EXPECT_HTTP_STATUS : TCPCHK_EXPECT_HTTP_STATUS_REGEX);
@@ -3027,7 +3027,7 @@ struct tcpcheck_rule *parse_tcpcheck_expect(char **args, int cur_arg, struct pro
 				goto error;
 			}
 			if (type != TCPCHK_EXPECT_UNDEF) {
-				memprintf(errmsg, "only on pattern expected");
+				memprintf(errmsg, "only one pattern expected");
 				goto error;
 			}
 			type = TCPCHK_EXPECT_CUSTOM;
@@ -3038,7 +3038,7 @@ struct tcpcheck_rule *parse_tcpcheck_expect(char **args, int cur_arg, struct pro
 			if (proto != TCPCHK_RULES_HTTP_CHK)
 				goto bad_tcp_kw;
 			if (type != TCPCHK_EXPECT_UNDEF) {
-				memprintf(errmsg, "only on pattern expected");
+				memprintf(errmsg, "only one pattern expected");
 				goto error;
 			}
 			type = TCPCHK_EXPECT_HTTP_HEADER;
