c4e14dc2a88634a5a4b29b3ce81ac822f2a48231
Christopher Faulet <cfaulet@haproxy.com>
2026-04-09 21:00:00 +0200

    BUG/MEDIUM: samples: Fix handling of SMP_T_METH samples
    Samples of type SMP_T_METH were not properly handled in smp_dup(),
    smp_is_safe() and smp_is_rw(). For "other" methods, for instance PATCH, a
    fallback was performed on the SMP_T_STR type. Only the buffer considered
    changed. "smp->data.u.meth.str" should be used for the SMP_T_METH samples
    while smp->data.u.str should be used for SMP_T_STR samples. However, in
    smp_dup(), the result was stored in wrong buffer, the string one instead of
    the method one. In smp_is_safe() and smp_is_rw(), the method buffer was not
    used at all.
    
    We now take care to use the right buffer.
    
    This patch must be backported to all stable versions.
    
    (cherry picked from commit b0a9216ca5375c076fd144db68365d7a9a41a161)
    [wt: s/get_trash_chunk_sz()/get_trash_chunk() in smp_dup()]
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 062afc3d14932cd9c88933b3ec336e26c3ce8125)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit bc6ebb5f35c07050edfc8581b9bb4c753a83f2ce)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 81f0618475f59ac146bfcaa683831d084e09c7ee)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit eb111af09b4bfa5d805dde18be5299040792b05c)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/sample.c
+++ src/sample.c
@@ -809,7 +809,7 @@ static int c_int2str(struct sample *smp)
  */
 int smp_dup(struct sample *smp)
 {
-	struct buffer *trash;
+	struct buffer *trash, *buf;
 
 	switch (smp->data.type) {
 	case SMP_T_BOOL:
@@ -826,17 +826,17 @@ int smp_dup(struct sample *smp)
 		/* Fall through */
 
 	case SMP_T_STR:
+		buf = (smp->data.type == SMP_T_STR ? &smp->data.u.str : &smp->data.u.meth.str);
 		trash = get_trash_chunk();
-		trash->data = smp->data.type == SMP_T_STR ?
-		    smp->data.u.str.data : smp->data.u.meth.str.data;
+		if (!trash)
+			return 0;
+		trash->data = buf->data;
 		if (trash->data > trash->size - 1)
 			trash->data = trash->size - 1;
 
-		memcpy(trash->area, smp->data.type == SMP_T_STR ?
-		    smp->data.u.str.area : smp->data.u.meth.str.area,
-		    trash->data);
+		memcpy(trash->area, buf->area, trash->data);
 		trash->area[trash->data] = 0;
-		smp->data.u.str = *trash;
+		*buf = *trash;
 		break;
 
 	case SMP_T_BIN:
