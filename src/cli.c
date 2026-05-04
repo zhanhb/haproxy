66e0e4539d225600237d0592133974580009f78d
William Lallemand <wlallemand@haproxy.com>
2026-05-04 19:00:27 +0200

    BUG/MINOR: mworker/cli: check ci_insert() return value in pcli_parse_request()
    All ci_insert() calls in pcli_parse_request() were ignoring the return
    value, silently miscounting the bytes to forward if an insertion failed.
    
    Add a check on each call and return -1 on failure. In practice this has
    no impact: the channel receive machinery enforces a maxrewrite reserve,
    so there is always sufficient room in the buffer for these small
    prefixes by the time pcli_parse_request() is called.
    
    Must be backported in every maintained versions, the list of available
    commands might change.
    
    (cherry picked from commit 166dfcbbeed348bbdb4798818e24bc99239a2aaf)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit fdfa9abca5932dfcaed4bcaeb7ef87d6d8a73e3d)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 9eb0f2e473e40cb419b0b72d5f3988dbc42675ac)
    [wt: adjusted to existing commands]
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit c432c40666e89e1b62dcfe67697459617c9bdc85)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit d6baf0ff6acfc658f086094b9f2a6fe6527d934e)
    [wt: s/ci_insert/ci_insert_line2]
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/cli.c
+++ src/cli.c
@@ -2595,35 +2595,56 @@ int pcli_parse_request(struct stream *s, struct channel *req, char **errmsg, int
 
 		/* the mcli-debug-mode is only sent to the applet of the master */
 		if ((s->pcli_flags & ACCESS_MCLI_DEBUG) && *next_pid <= 0) {
-			ci_insert_line2(req, 0, "mcli-debug-mode on -", strlen("mcli-debug-mode on -"));
+			if (!ci_insert_line2(req, 0, "mcli-debug-mode on -", strlen("mcli-debug-mode on -"))) {
+				ret = -1;
+				goto end;
+			}
 			ret += strlen("mcli-debug-mode on -") + 2;
 		}
 		if (s->pcli_flags & ACCESS_EXPERIMENTAL) {
-			ci_insert_line2(req, 0, "experimental-mode on -", strlen("experimental-mode on -"));
+			if (!ci_insert_line2(req, 0, "experimental-mode on -", strlen("experimental-mode on -"))) {
+				ret = -1;
+				goto end;
+			}
 			ret += strlen("experimental-mode on -") + 2;
 		}
 		if (s->pcli_flags & ACCESS_EXPERT) {
-			ci_insert_line2(req, 0, "expert-mode on -", strlen("expert-mode on -"));
+			if (!ci_insert_line2(req, 0, "expert-mode on -", strlen("expert-mode on -"))) {
+				ret = -1;
+				goto end;
+			}
 			ret += strlen("expert-mode on -") + 2;
 		}
 		if (s->pcli_flags & ACCESS_MCLI_SEVERITY_STR) {
 			const char *cmd = "set severity-output string -";
-			ci_insert_line2(req, 0, cmd, strlen(cmd));
+			if (!ci_insert_line2(req, 0, cmd, strlen(cmd))) {
+				ret = -1;
+				goto end;
+			}
 			ret += strlen(cmd) + 2;
 		}
 		if (s->pcli_flags & ACCESS_MCLI_SEVERITY_NB) {
 			const char *cmd = "set severity-output number -";
-			ci_insert_line2(req, 0, cmd, strlen(cmd));
+			if (!ci_insert_line2(req, 0, cmd, strlen(cmd))) {
+				ret = -1;
+				goto end;
+			}
 			ret += strlen(cmd) + 2;
 		}
 
 		if (pcli_has_level(s, ACCESS_LVL_ADMIN)) {
 			goto end;
 		} else if (pcli_has_level(s, ACCESS_LVL_OPER)) {
-			ci_insert_line2(req, 0, "operator -", strlen("operator -"));
+			if (!ci_insert_line2(req, 0, "operator -", strlen("operator -"))) {
+				ret = -1;
+				goto end;
+			}
 			ret += strlen("operator -") + 2;
 		} else if (pcli_has_level(s, ACCESS_LVL_USER)) {
-			ci_insert_line2(req, 0, "user -", strlen("user -"));
+			if (!ci_insert_line2(req, 0, "user -", strlen("user -"))) {
+				ret = -1;
+				goto end;
+			}
 			ret += strlen("user -") + 2;
 		}
 	}
