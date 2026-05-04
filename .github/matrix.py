zhanhb/haproxy

commit 8cfe0bb9a97aa62db6b9e89abdc3ab65f03462d4

CI: github: only enable OS X on development branches
Don't use the macOS job on maintenance branches, it's mainly use for
development and checking portability, but we don't support actively
macOS on stable branches.

(cherry picked from commit 41a71aec3d0340749e9a74fc304873c62ede9456)
[wt: backported to fix the stable CI. We don't care about macos version
 here since it's only used in -dev]
Signed-off-by: Willy Tarreau <w@1wt.eu>
(cherry picked from commit 6f7d84e713cf4c7403e6b2e2adef72ade630f4b3)
[wt: adjusted context]
Signed-off-by: Willy Tarreau <w@1wt.eu>
(cherry picked from commit 2dc16be526f844905b4323f1dafbb3b82e69aa72)
[wt: adjusted context]
Signed-off-by: Willy Tarreau <w@1wt.eu>
(cherry picked from commit 74df1bad5c38957d899a9cd6c728bd2336ffb9ff)
[wt: the macos block was out of the loop in 2.8]
Signed-off-by: Willy Tarreau <w@1wt.eu>
(cherry picked from commit 550119e9879b07685e58e8dd271f06e06f5313a5)
[wt: updated the macos block again (different in 2.6)]
Signed-off-by: Willy Tarreau <w@1wt.eu>
(cherry picked from commit 652c8c8a9f6e303b3cac0bad1bd39a2d21bb6f9b)
[wt: adjusted ctx]
Signed-off-by: Willy Tarreau <w@1wt.eu>

.github/matrix.py
@@ -191,20 +191,20 @@ for CC in ["gcc","clang"]:
         }
     )
 
-# macOS
-
-os = "macos-latest"
-TARGET = "osx"
-for CC in ["clang"]:
-    matrix.append(
-        {
-            "name": "{}, {}, no features".format(clean_os(os), CC),
-            "os": os,
-            "TARGET": TARGET,
-            "CC": CC,
-            "FLAGS": [],
-        }
-    )
+# macOS on dev branches
+if "haproxy-" not in ref_name:
+    os = "macos-26"     # development branch
+    TARGET = "osx"
+    for CC in ["clang"]:
+        matrix.append(
+            {
+                "name": "{}, {}, no features".format(os, CC),
+                "os": os,
+                "TARGET": TARGET,
+                "CC": CC,
+                "FLAGS": [],
+            }
+        )
 
 # Print matrix
 

commit 2493d0ef35628d24f02df5775056d8ef62ad10f6

CI: github: fix build matrix after latest osx backport
Commit 8cfe0bb9a9 ("CI: github: only enable OS X on development branches")
broke the build matrix because ref_name is not present in the 2.4 github
script. Let's fix it.

.github/matrix.py
@@ -192,19 +192,18 @@ for CC in ["gcc","clang"]:
     )
 
 # macOS on dev branches
-if "haproxy-" not in ref_name:
-    os = "macos-26"     # development branch
-    TARGET = "osx"
-    for CC in ["clang"]:
-        matrix.append(
-            {
-                "name": "{}, {}, no features".format(os, CC),
-                "os": os,
-                "TARGET": TARGET,
-                "CC": CC,
-                "FLAGS": [],
-            }
-        )
+os = "macos-26"     # development branch
+TARGET = "osx"
+for CC in ["clang"]:
+    matrix.append(
+        {
+            "name": "{}, {}, no features".format(os, CC),
+            "os": os,
+            "TARGET": TARGET,
+            "CC": CC,
+            "FLAGS": [],
+        }
+    )
 
 # Print matrix
 