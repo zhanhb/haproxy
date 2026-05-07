4cf8a3871e606fd300785a01c51a3ffb6b462e5b
Willy Tarreau <w@1wt.eu>
2026-05-07 16:28:46 +0200

    Revert "BUG/MEDIUM: cli: fix master CLI connection slot leak on client disconnect"
    This reverts commit 64383e655b23e1240dd0043a18ca020994c60022.
    
    As reported by Alexander Stephan in issue #3351, it causes problems.
    First, as seen in the issue, the "reload" operation, handled by an applet
    local to the master process, is being interrupted by the timeout so that
    the client never gets the result (though the timeout is applied). A fix
    for this was found (ignore client-fin/server-fin on applets, as they make
    no sense there), but it only hides a deeper problem. Indeed, issuing
    "@1 debug dev delay 2000" still stops at 1s with an error, indicating
    that commands are systematically being sent with a shutdown, and thus
    that the server-fin always applies. This is a problem because it means
    that any long command will now be interrupted after one second.
    
    All of this needs to be put back into perspective before progressing
    further on this issue, and the reason for sending the shutdown should
    be reconsidered in the context of the current version, as it looks
    like this was once necessary but no longer is.
    
    In addition, the issue encountered by Alexander, of a frozen worker,
    was essentially reported once in many years, so it's totally acceptable
    to leave older versions unfixed and figure what's the best solution for
    modern versions only.
    
    Let's just revert to the pre-fix situation so as to avoid causing
    breakage everywhere. This revert should be backported to all versions
    (2.4 included).
    
    (cherry picked from commit 782336c21bdfd5635b3098515643abf9ea60afab)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 12986597111698ad994a86402a0305fe4f8ac82a)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 42e8e4e1e0bece7423c44ab9df70f6a4cdb89500)
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    (cherry picked from commit 292c6b27abe4331bc87d89bb5d8aa2d79a8613fe)
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- src/cli.c
+++ src/cli.c
@@ -3100,9 +3100,8 @@ int mworker_cli_proxy_create()
 	mworker_proxy->mode = PR_MODE_CLI;
 	/* default to 10 concurrent connections */
 	mworker_proxy->maxconn = 10;
-	mworker_proxy->timeout.client = 0; /* no timeout */
-	mworker_proxy->timeout.serverfin = MS_TO_TICKS(1000); /* 1s timeout in case worker is not responding on shutdown */
-
+	/* no timeout */
+	mworker_proxy->timeout.client = 0;
 	mworker_proxy->conf.file = strdup("MASTER");
 	mworker_proxy->conf.line = 0;
 	mworker_proxy->accept = frontend_accept;
