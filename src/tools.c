8d822625a4ac30d8435bf6ef0a652bd5d730e655
Willy Tarreau <w@1wt.eu>
2026-05-11 15:04:19 +0200

    BUG/MINOR: tools: fix memory leak in env_expand() error path
    When my_realloc2() fails in env_expand(), the code jumps to 'leave:' and
    returns NULL, but the original input 'in' is never freed (it's only freed
    at line 4919 in the success case). Given that callers typically pass it
    the direct return of strdup(), it looks like it is expected to always be
    freed. This can be backported everywhere.
    
    (cherry picked from commit 0995c914bd6406264e24b44278bbc806c43ba67c)
    Signed-off-by: William Lallemand <wlallemand@haproxy.com>
    (cherry picked from commit 312ed64f5c921470df3c475f98f35e319a1e6efc)
    Signed-off-by: William Lallemand <wlallemand@haproxy.com>
    (cherry picked from commit eae0604ea4482e7617b9c29279d9454ec6b6338a)
    Signed-off-by: William Lallemand <wlallemand@haproxy.com>

--- src/tools.c
+++ src/tools.c
@@ -4721,8 +4721,10 @@ char *env_expand(char *in)
 		}
 
 		out = my_realloc2(out, out_len + (txt_end - txt_beg) + val_len + 1);
-		if (!out)
+		if (!out) {
+			free(in);
 			goto leave;
+		}
 
 		if (txt_end > txt_beg) {
 			memcpy(out + out_len, txt_beg, txt_end - txt_beg);
