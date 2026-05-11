6df4babcae3821b1bcecc99b6163de9afa826629
Willy Tarreau <w@1wt.eu>
2026-05-11 14:58:45 +0200

    BUG/MINOR: http-fetch: make http_first_req() check for HTTP first
    smp_fetch_http_first_req() reads ->txn.http->flags without first
    checking if txn.http is properly allocated. In theory if called from
    the wrong context it could crash, even though tests where it's called
    from "tcp-request content" don't seem to have any effect. Let's fix
    it regardless, at least to dissipate the doubt. It can be backported
    everywhere.
    
    (cherry picked from commit 8941cc5f6db3b86ef68d88f7305acc82069887b4)
    Signed-off-by: William Lallemand <wlallemand@haproxy.com>
    (cherry picked from commit cdc9b7dc8c4d06d8995242a761f417a32bd96957)
    Signed-off-by: William Lallemand <wlallemand@haproxy.com>

--- src/http_fetch.c
+++ src/http_fetch.c
@@ -1315,7 +1315,7 @@ static int smp_fetch_proto_http(const struct arg *args, struct sample *smp, cons
 /* return a valid test if the current request is the first one on the connection */
 static int smp_fetch_http_first_req(const struct arg *args, struct sample *smp, const char *kw, void *private)
 {
-	if (!smp->strm)
+	if (!smp->strm || !smp->strm->txn)
 		return 0;
 
 	smp->data.type = SMP_T_BOOL;
