bd22259bb740ce859289e2084f58d25a46502e91
Christopher Faulet <cfaulet@haproxy.com>
2026-05-26 15:35:46 +0200

    BUG/MEDIUM: h1-htx: Sanitize parsing to properly handle upgrade requests
    Thanks to previous patches, the request messages are now sanitized to
    properly handle Upgrade requests. Now, if a 'connection: upgrade' header
    value was found while no 'Upgrade' header, the 'upgrade' values is removed
    from the 'connection' header. Conversely the opposite is also performed. If
    'Upgrade' header was found, but no "conneciotn: upgrade" header value, all
    occurrences of 'Upgrade' header are refused.
    
    This patch depends on following ones:
      * MINOR: h1: Add  a H1M flag to specify a non-empty 'Upgrade:' header was parsed
      * MINOR: http: Add function to remove all occurrences of a value in a header
    
    It should fix the issue 3397. But the H2 part should be reviewed too, and
    probably the H1 response parsing, to be consistent with this change.
    
    The series should be backported as far as 2.4.
    
    (cherry picked from commit 3843f48faf3aee2cee827d5ae93bf72ba7f86530)
    [cf: Context adjustment]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- include/haproxy/htx-t.h
+++ include/haproxy/htx-t.h
@@ -141,6 +141,7 @@
 #define HTX_SL_F_NORMALIZED_URI 0x00000800 /* The received URI is normalized (an implicit absolute-uri form) */
 #define HTX_SL_F_CONN_UPG       0x00001000 /* The message contains "connection: upgrade" header */
 #define HTX_SL_F_BODYLESS_RESP  0x00002000 /* The response to this message is bodyloess (only for reqyest) */
+#define HTX_SL_F_UPG_HDR        0x00008000 /* non-empty Upgrapde header found */
 
 /* This function is used to report flags in debugging tools. Please reflect
  * below any single-bit flag addition above in the same order via the
@@ -157,7 +158,8 @@ static forceinline char *hsl_show_flags(char *buf, size_t len, const char *delim
 	_(HTX_SL_F_CLEN, _(HTX_SL_F_CHNK, _(HTX_SL_F_VER_11,
 	_(HTX_SL_F_BODYLESS, _(HTX_SL_F_HAS_SCHM, _(HTX_SL_F_SCHM_HTTP,
 	_(HTX_SL_F_SCHM_HTTPS, _(HTX_SL_F_HAS_AUTHORITY,
-	_(HTX_SL_F_NORMALIZED_URI, _(HTX_SL_F_CONN_UPG)))))))))))));
+	_(HTX_SL_F_NORMALIZED_URI, _(HTX_SL_F_CONN_UPG, _(HTX_SL_F_BODYLESS_RESP,
+	_(HTX_SL_F_UPG_HDR)))))))))))))));
 	/* epilogue */
 	_(~0U);
 	return buf;
