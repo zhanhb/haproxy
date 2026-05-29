fd5decd1ce7bf42929e20b2282d7a20e333f6e6e
Christopher Faulet <cfaulet@haproxy.com>
2026-05-29 16:26:29 +0200

    BUG/MEDIUM: mux-h1: Dup connection/upgrade value to parse it when making headers
    When message headers are formatted, the connection and upgrade header values
    are parsed to be sanitized and to fill H1M flags. The values are modified in
    place without changing the HTX message information accordingly (the block
    info and the HTX info). It could be an issue if the output buffer is full
    and the header cannot be formatted. Because the formatting can be stopped
    with a HTX message in hazardous state.
    
    It should be quite difficult to trigger this issue. But now, a copy of the
    value is performed before parsing it. So only the copy will be altered,
    leaving the HTX message in a safe state.
    
    This patch must be backported to all stable versions.
    
    (cherry picked from commit de25313cd8c58f120d52ddb57a26fff5ca4951c6)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 9947d654c0dc9da3deda7e4183f12fe519d7a76b)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit b7c68d5823d0875a18625b8d45971d013572dcd9)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 0834e38d0011ccfdf38a88ec1df02bb7ea46c86b)
    [cf: Changes are inlined in h1_process_mux()]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/mux_h1.c
+++ src/mux_h1.c
@@ -2194,11 +2194,17 @@ static size_t h1_process_mux(struct h1c *h1c, struct buffer *buf, size_t count)
 						goto skip_hdr;
 				}
 				else if (isteq(n, ist("connection"))) {
+					/* copy the value because it can be modified, but the HTX blocks will not */
+					memcpy(trash.area, v.ptr, v.len);
+					v.ptr = trash.area;
 					h1_parse_connection_header(h1m, &v);
 					if (!v.len)
 						goto skip_hdr;
 				}
 				else if (isteq(n, ist("upgrade"))) {
+					/* copy the value because it can be modified, but the HTX blocks will not */
+					memcpy(trash.area, v.ptr, v.len);
+					v.ptr = trash.area;
 					h1_parse_upgrade_header(h1m, &v);
 					if (!v.len)
 						goto skip_hdr;
