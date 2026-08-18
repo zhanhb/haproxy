d3dd82cbe83b3f662c9662f7860a8852e5ede9e4
William Lallemand <wlallemand@haproxy.com>
2026-08-18 12:01:55 +0000

    BUILD: ssl: disable the TLS1.3 session timeout clamp on wolfSSL
    wolfSSL's OpenSSL compat layer declares wolfSSL_get_SSL_CTX() (aka
    SSL_get_SSL_CTX()) taking a non-const WOLFSSL*, unlike OpenSSL's
    const-correct SSL_get_SSL_CTX(const SSL *ssl). Calling it from
    ssl_sock_infocbk(), which only has a const SSL *, fails the build:
    
      src/ssl_sock.c:1772:60: error: passing 'const SSL *' (aka 'const
      struct WOLFSSL *') to parameter of type 'WOLFSSL *' (aka 'struct
      WOLFSSL *') discards qualifiers
      [-Werror,-Wincompatible-pointer-types-discards-qualifiers]
    
    Beyond the build failure, the clamp itself doesn't appear to behave
    correctly against wolfSSL's TLS1.3 session/ticket handling, so skip
    the whole thing on wolfSSL builds instead of only working around the
    compile error.
    
    Must be backported in every stable branches along with 9f1d590
    ("BUG/MEDIUM: ssl: enforce tune.ssl.lifetime across TLS1.3 session
    renewals")
    
    (cherry picked from commit 379a07153a80d0c276e5956f5fe18b530b03fbe2)
    Signed-off-by: William Lallemand <wlallemand@haproxy.com>
    (cherry picked from commit 1d21bfa2937618eaf44b86ef1a2b2e3b1fcfaf67)
    Signed-off-by: William Lallemand <wlallemand@haproxy.com>
    (cherry picked from commit bd933638479fe39487a3fd86ecda342f6cc42a0c)
    Signed-off-by: William Lallemand <wlallemand@haproxy.com>

--- src/ssl_sock.c
+++ src/ssl_sock.c
@@ -1483,7 +1483,7 @@ void ssl_sock_infocbk(const SSL *ssl, int where, int ret)
 			}
 		}
 
-#ifdef TLS1_3_VERSION
+#if defined(TLS1_3_VERSION) && !defined(USE_OPENSSL_WOLFSSL)
 	/* Reduce TLS1.3 session timeout so it doesn't reset at each new
 	 * resumed connection */
 
