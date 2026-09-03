565b7f84820e4955588bcd0c1b75768ab07807ab
Olivier Houchard <ohouchard@haproxy.com>
2026-09-03 13:39:04 +0200

    MEDIUM: mux-h1: Harden test for short data at end of stream
    Commit 75dd2c18dda29f9893574ef982ddf66de16f0408 made it so if we had a
    content-length, and we sent less data than expected, we'd make sure to
    close the connection and to not reuse it, otherwise the server would
    still expect more data. But it can also happen with no content-length,
    with chunked data. There is absolutely no reason we reach that code
    while still expecting more data, so just always consider that an error,
    no matter if we have content-length or not.
    
    This should be backported up to 2.8.
    
    (cherry picked from commit 4721a96f161b83cbb5015d15821e84e16fe21151)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 0bcd9f9c90f638d5b102f6200a7b7d114ba3457c)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 8c935105e92999f7a4c3a2a70517143955ca20a7)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 7bcc5393c3c3ef4a67983797f548a2ec55f07882)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 29c8e8b334156ff8806cb328e238c273e0d4feec)
    [cf: Adapted because of a missing backport. But also because
         H1S_F_BODYLESS_REQ flag does not exist]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/mux_h1.c
+++ src/mux_h1.c
@@ -863,6 +863,15 @@ static void h1s_destroy(struct h1s *h1s)
 				H1C_F_OUT_FULL|H1C_F_OUT_ALLOC|H1C_F_IN_SALLOC|
 				H1C_F_CO_MSG_MORE|H1C_F_CO_STREAMER);
 
+		/*
+		 * If we reached the end of the stream and sent less bits than
+		 * announced in Content-Length, we have little choice but to
+		 * close the connection, as the peer will expect more.
+		 */
+		if (h1s->req.curr_len ||
+		    (h1s->res.curr_len && !(h1s->flags & H1S_F_BODYLESS_RESP)))
+			h1c->flags |= H1C_F_ERROR;
+
 		if (!(h1c->flags & (H1C_F_EOS|H1C_F_ERR_PENDING|H1C_F_ERROR|H1C_F_ABRT_PENDING|H1C_F_ABRTED)) &&  /* No error/read0/abort */
 		    h1_is_alive(h1c) &&                                                      /* still alive */
 		    (h1s->flags & H1S_F_WANT_KAL) &&                                         /* K/A possible */
