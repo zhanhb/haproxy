7a501951104b052677837ecfd97c5222fd69f976
Amaury Denoyelle <adenoyelle@haproxy.com>
2026-09-03 17:20:15 +0200

    BUG/MEDIUM: h3: do not block FIN on empty DATA frame
    A HTTP/3 DATA frame may be empty as its payload is entirely optional.
    However, prior to this patch, H3 stream parsing was interrupted in this
    case, unless other frames follow. The main problem is that it would lead
    to a missing EOM if QUIC FIN is reported with an empty DATA frame,
    causing the stream to freeze until timeout.
    
    The first step of this patch is to first remove the early return for
    empty HTTP/3 frame payload. This has the effect to allow calling the
    different frame handlers with an empty payload. The current patch is
    dedicated to HTTP/3 DATA support; other patches will follow to ensure
    the safety of the other frame types handlers.
    
    The second part of this patch is to adjust h3_data_to_htx() which is the
    HTTP/3 data frame handler. It is now safe to call it with an empty
    frame, with or without FIN. It will default to a noop operation, unless
    FIN is set, in which case htx_set_eom() is used to report the closure of
    the stream read channel.
    
    This must be backported up to 2.6.
    
    (cherry picked from commit 6c706c1a758b1e8bcdfe0816535edbd5c624db80)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 7f680ec6f79259c6253ed7541b311e763a1f62b2)
    [cf: Adapted because there is no term events support on 3.3]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit fb33c1080a6feaa29bf75955488281129218208f)
    [cf: Adapted because the backport on 3.3 was partially mangeled with the
         previous one. But no issue at the end]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 99bfcad50fc1be4f58c4c910c6ed7c29c18083db)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 350b42a262157be1ebeeba13c6d7cd3ab4261e81)
    [cf: Error handling was adapted]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/h3.c
+++ src/h3.c
@@ -1125,13 +1125,21 @@ static ssize_t h3_data_to_htx(struct qcs *qcs, const struct buffer *buf,
 {
 	struct h3s *h3s = qcs->ctx;
 	struct h3c *h3c = h3s->h3c;
-	struct buffer *appbuf;
+	struct buffer *appbuf = NULL;
 	struct htx *htx = NULL;
 	size_t htx_sent = 0;
 	int htx_space;
 	char *head;
 
 	TRACE_ENTER(H3_EV_RX_FRAME|H3_EV_RX_DATA, qcs->qcc->conn, qcs);
+
+	if (!len && !fin) {
+		/* Ignore empty frame to prevent unnecessary appbuf alloc. Not
+		 * performed though if FIN is set as HTX EOM must be reported.
+		 */
+		TRACE_STATE("skip parsing of empty DATA frame", H3_EV_RX_FRAME, qcs->qcc->conn, qcs);
+		goto out;
+	}
 
 	if (!(appbuf = qcs_get_buf(qcs, &qcs->rx.app_buf))) {
 		TRACE_ERROR("data buffer alloc failure", H3_EV_RX_FRAME|H3_EV_RX_DATA, qcs->qcc->conn, qcs);
@@ -1141,6 +1149,19 @@ static ssize_t h3_data_to_htx(struct qcs *qcs, const struct buffer *buf,
 	}
 
 	htx = htx_from_buf(appbuf);
+
+	if (!len) {
+		/* Ensure EOM is reported when an empty frame is handled. */
+		if (!htx_set_eom(htx)) {
+			TRACE_ERROR("cannot set EOM", H3_EV_RX_FRAME, qcs->qcc->conn, qcs);
+			h3c->err = H3_INTERNAL_ERROR;
+			len = -1;
+			goto out;
+		}
+
+		TRACE_STATE("EOM reported after empty frame", H3_EV_RX_FRAME, qcs->qcc->conn, qcs);
+		goto out;
+	}
 
 	if (len > b_data(buf)) {
 		len = b_data(buf);
@@ -1378,9 +1399,6 @@ static ssize_t h3_decode_qcs(struct qcs *qcs, struct buffer *b, int fin)
 				qcc_set_error(qcs->qcc, H3_FRAME_UNEXPECTED, 1);
 				goto err;
 			}
-
-			if (!b_data(b))
-				break;
 		}
 
 		flen = h3s->demux_frame_len;
