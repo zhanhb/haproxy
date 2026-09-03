fb33c1080a6feaa29bf75955488281129218208f
Amaury Denoyelle <adenoyelle@haproxy.com>
2026-09-03 17:20:15 +0200

    BUG/MEDIUM: h3: do not block FIN on empty DATA frame
    A HTTP/3 DATA frame may be empty as its payload is entirely optional.
    However, prior to this patch, H3 stream parsing was interrupted in this
    case, unless other frames follow. The main problem is that it would lead
    to a missing EOM if QUIC FIN is reported with an empty DATA frame,
    causing the stream to freeze until timeout.
    
    The first step of this patch is to first remove the early return for
    empty HTTP/3 frame payload. This has the effect to allow calling the
    different frame handlers with an empty payload. The current patch is
    dedicated to HTTP/3 DATA support; other patches will follow to ensure
    the safety of the other frame types handlers.
    
    The second part of this patch is to adjust h3_data_to_htx() which is the
    HTTP/3 data frame handler. It is now safe to call it with an empty
    frame, with or without FIN. It will default to a noop operation, unless
    FIN is set, in which case htx_set_eom() is used to report the closure of
    the stream read channel.
    
    This must be backported up to 2.6.
    
    (cherry picked from commit 6c706c1a758b1e8bcdfe0816535edbd5c624db80)
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 7f680ec6f79259c6253ed7541b311e763a1f62b2)
    [cf: Adapted because there is no term events support on 3.3]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/h3.c
+++ src/h3.c
@@ -1593,7 +1593,7 @@ static ssize_t h3_trailers_to_htx(struct qcs *qcs, const struct buffer *buf,
 static ssize_t h3_data_to_htx(struct qcs *qcs, const struct buffer *buf,
                               uint64_t len, char fin)
 {
-	struct buffer *appbuf;
+	struct buffer *appbuf = NULL;
 	struct htx *htx = NULL;
 	size_t htx_sent = 0;
 	int htx_space;
@@ -1601,12 +1601,32 @@ static ssize_t h3_data_to_htx(struct qcs *qcs, const struct buffer *buf,
 
 	TRACE_ENTER(H3_EV_RX_FRAME|H3_EV_RX_DATA, qcs->qcc->conn, qcs);
 
+	if (!len && !fin) {
+		/* Ignore empty frame to prevent unnecessary appbuf alloc. Not
+		 * performed though if FIN is set as HTX EOM must be reported.
+		 */
+		TRACE_STATE("skip parsing of empty DATA frame", H3_EV_RX_FRAME, qcs->qcc->conn, qcs);
+		goto out;
+	}
+
 	if (!(appbuf = qcc_get_stream_rxbuf(qcs))) {
 		TRACE_ERROR("data buffer alloc failure", H3_EV_RX_FRAME|H3_EV_RX_DATA, qcs->qcc->conn, qcs);
 		goto err;
 	}
 
 	htx = htx_from_buf(appbuf);
+
+	if (!len) {
+		/* Ensure EOM is reported when an empty frame is handled. */
+		if (!htx_set_eom(htx)) {
+			TRACE_ERROR("cannot set EOM", H3_EV_RX_FRAME, qcs->qcc->conn, qcs);
+			qcc_set_error(qcs->qcc, H3_ERR_INTERNAL_ERROR, 1);
+			goto err;
+		}
+
+		TRACE_STATE("EOM reported after empty frame", H3_EV_RX_FRAME, qcs->qcc->conn, qcs);
+		goto out;
+	}
 
 	if (len > b_data(buf)) {
 		len = b_data(buf);
@@ -1871,9 +1891,6 @@ static ssize_t h3_rcv_buf(struct qcs *qcs, struct buffer *b, int fin)
 					qcs->sd->kip += flen;
 				}
 			}
-
-			if (!b_data(b))
-				break;
 		}
 
 		/* RFC 9114 7.1. Frame Layout
@@ -2058,7 +2075,7 @@ static ssize_t h3_rcv_buf(struct qcs *qcs, struct buffer *b, int fin)
 				 * type H3_FRAME_ERROR.
 				 */
 				TRACE_ERROR("truncated MAX_PUSH_ID frame", H3_EV_RX_FRAME, qcs->qcc->conn, qcs);
-				qcc_set_error(qcs->qcc, H3_ERR_FRAME_ERROR, 1, muxc_tevt_type_proto_err);
+				qcc_set_error(qcs->qcc, H3_ERR_FRAME_ERROR, 1);
 				qcc_report_glitch(qcs->qcc, 1);
 				goto err;
 			}
