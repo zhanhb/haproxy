c961b0f02ac49a7f69bf48f9be7803fb5efd40ab
Christopher Faulet <cfaulet@haproxy.com>
2026-09-23 04:31:28 +0000

    BUG/MEDIUM: log: reserve the trailing 0 in CBOR int and bool encoders
    Commit 378386e14 ("BUG/MEDIUM: log: always reserve room for trailing 0 when
    using CBOR encoding helpers") made the text encoding helpers reserve one
    byte for the terminating NULL byte that sess_build_logline_orig() always
    appends, but lf_bool_encode() and lf_int_encode() were missed:
    _lf_cbor_encode_byte() and cbor_encode_int64_{bin,hex}() were still called
    with "dst + size" as the stop pointer. So the trailing 0 could be written
    one byte past the end of the output buffer.
    
    Let's pass "dst + size - 1" to these encoders like the other helpers do.
    The log_cbor reg-test was extended to sweep a string across the end of the
    chunk, followed by an int or a bool, with pool tags enabled.
    
    The was introduced by c614fd3b9 ("MINOR: log: add +cbor encoding
    option"), so this must be backported up to 3.0.
    
    (cherry picked from commit cf9670997ad9ad018ad5e85bd1542e5c186ade7f)
    [cf: Adapted to the context]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/log.c
+++ src/log.c
@@ -2625,9 +2625,12 @@ static char *lf_bool_encode(char *dst, size_t size, uint8_t value,
 		return ret;
 	}
 	if (ctx->options & LOG_OPT_ENCODE_CBOR) {
+		/* the cbor encoder doesn't append terminating NULL byte, we
+		 * must reserve 1 byte for that.
+		 */
 		if (value)
-			return _lf_cbor_encode_byte(&ctx->encode.cbor, dst, dst + size, 0xF5);
-		return _lf_cbor_encode_byte(&ctx->encode.cbor, dst, dst + size, 0xF4);
+			return _lf_cbor_encode_byte(&ctx->encode.cbor, dst, dst + size -1, 0xF5);
+		return _lf_cbor_encode_byte(&ctx->encode.cbor, dst, dst + size - 1, 0xF4);
 	}
 
 	return NULL; /* not supported */
@@ -2668,9 +2671,10 @@ static char *lf_int_encode(char *dst, size_t size, int64_t value,
 	}
 	else if (ctx->options & LOG_OPT_ENCODE_CBOR) {
 		/* Always print as a regular int64 number (STR typecast isn't
-		 * supported)
+		 * supported). The cbor encoder doesn't append terminating NULL
+		 * byte, we must reserve 1 byte for that.
 		 */
-		return cbor_encode_int64(&ctx->encode.cbor, dst, dst + size, value);
+		return cbor_encode_int64(&ctx->encode.cbor, dst, dst + size - 1, value);
 	}
 
 	return NULL; /* not supported */
