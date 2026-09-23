f3bd6312403dfd37131fa3e314ebbb3960f7bbd8
Christopher Faulet <cfaulet@haproxy.com>
2026-09-23 05:09:08 +0000

    BUG/MINOR: http-ana: restore the transaction status after early hints
    While a 103 response is being built by consecutive early-hint rules, the
    transaction status is set to 103 so that the next rules append their headers
    to the same response. Since commit 4c3d3d2a6 ("BUG/MINOR: http-act: Properly
    generate 103 responses when several rules are used"), once the response was
    finished, or on error, the status was reset to 0.
    
    But the initial status of a transaction is -1, and some code relies on it to
    know whether a status was set. This is the case of the deny handling in
    http_req_get_intercept_rule(), which only uses the default 403 status if the
    status is still -1. So a Lua action returning act.DENY after an early-hint
    rule makes haproxy answered with a 500 instead of a 403, and logged a null
    status.
    
    Let's restore -1 instead in http_early_hint_end() and on the error paths of
    the early-hint action and of the cache early hints. A Lua reg-test is added.
    
    This must be backported to all supported versions. The cache part only
    exists in 3.5.
    
    (cherry picked from commit ad09982e8817c927c67b8f5c5a099d4d637f7955)
    [cf: Adapted to the context. Cache part was removed. In addition,
         http_early_hint_end() does not exist.]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>
    (cherry picked from commit 64f60a3976229727e8f3cf1696874d4a033b0dbe)
    [cf: s->txn->status instead of s->txn.http->status]
    Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

--- src/http_act.c
+++ src/http_act.c
@@ -1407,7 +1407,7 @@ static enum act_return http_action_early_hint(struct act_rule *rule, struct prox
 			goto error;
 		if (!http_forward_proxy_resp(s, 0))
 			goto error;
-		s->txn->status = 0;
+		s->txn->status = -1;
 	}
 
   leave:
@@ -1424,7 +1424,7 @@ static enum act_return http_action_early_hint(struct act_rule *rule, struct prox
 	 * HTTP 103 response from the buffer */
 	channel_htx_truncate(res, htx);
 	ret = ACT_RET_ERR;
-	s->txn->status = 0;
+	s->txn->status = -1;
 	goto leave;
 }
 
